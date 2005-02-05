a3f613014d808f5517aaa8e882c6ffb74274778e
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2005-02-05 01:41:10 +0000

    r194: Making putchar() consistent with printf() and output to stdout.

---
+++ lib/console.c
@@ -0,0 +1,162 @@
+
+#include <inc/string.h>
+#include <inc/lib.h>
+
+void
+putchar(int ch)
+{
+	unsigned char c = ch;
+
+	write(1, &c, 1);
+}
+
+int
+getchar(void)
+{
+	unsigned char c;
+	int r;
+
+	r = read(0, &c, 1);
+	if (r < 0)
+		return r;
+	if (r < 1)
+		return -E_EOF;
+	return c;
+}
+
+int
+getchar_nb(void)
+{
+	unsigned char c;
+	int r;
+
+	r = read_nb(0, &c, 1);
+	if (r < 0)
+		return r;
+	if (r < 1)
+		return -E_EOF;
+	return c;
+}
+
+// "Real" console file descriptor implementation.
+// The putchar/getchar functions above will still come here by default,
+// but now can be redirected to files, pipes, etc., via the fd layer.
+
+static int cons_read(struct Fd*, void*, size_t, off_t);
+static int cons_read_nb(struct Fd*, void*, size_t, off_t);
+static int cons_write(struct Fd*, const void*, size_t, off_t);
+static int cons_close(struct Fd*);
+static int cons_stat(struct Fd*, struct Stat*);
+
+struct Dev devcons =
+{
+	.dev_id =	'c',
+	.dev_name =	"cons",
+	.dev_read =	cons_read,
+	.dev_read_nb =	cons_read_nb,
+	.dev_write =	cons_write,
+	.dev_close =	cons_close,
+	.dev_stat =	cons_stat
+};
+
+int
+iscons(int fdnum)
+{
+	int r;
+	struct Fd *fd;
+
+	if ((r = fd_lookup(fdnum, &fd)) < 0)
+		return r;
+	return fd->fd_dev_id == devcons.dev_id;
+}
+
+int
+opencons(void)
+{
+	int r;
+	struct Fd* fd;
+
+	if ((r = fd_alloc(&fd)) < 0)
+		return r;
+	if ((r = sys_page_alloc(0, fd, PTE_P|PTE_U|PTE_W|PTE_SHARE)) < 0)
+		return r;
+	fd->fd_dev_id = devcons.dev_id;
+	fd->fd_omode = O_RDWR;
+	return fd2num(fd);
+}
+
+int
+cons_read(struct Fd* fd, void* vbuf, size_t n, off_t offset)
+{
+	int c;
+
+	USED(offset);
+
+	if (n == 0)
+		return 0;
+
+	while ((c = sys_cgetc_nb()) == -1)
+		sys_yield();
+	if (c < 0)
+		return c;
+	if (c == 0x04)	// ctl-d is eof
+		return 0;
+	*(char*)vbuf = c;
+	return 1;
+}
+
+int
+cons_read_nb(struct Fd* fd, void* vbuf, size_t n, off_t offset)
+{
+	int c;
+
+	USED(offset);
+
+	if (n == 0)
+		return 0;
+
+	c = sys_cgetc_nb();
+	if (c < 0)
+		return c;
+	if (c == 0x04)	// ctl-d is eof
+		return 0;
+	*(char*)vbuf = c;
+	return 1;
+}
+
+int
+cons_write(struct Fd *fd, const void *vbuf, size_t n, off_t offset)
+{
+	int tot, m;
+	char buf[128];
+
+	USED(offset);
+
+	// mistake: have to nul-terminate arg to sys_cputs, 
+	// so we have to copy vbuf into buf in chunks and nul-terminate.
+	for (tot = 0; tot < n; tot += m) {
+		m = n - tot;
+		if (m > sizeof(buf) - 1)
+			m = sizeof(buf) - 1;
+		memcpy(buf, (char*)vbuf + tot, m);
+		buf[m] = 0;
+		sys_cputs(buf);
+	}
+	return tot;
+}
+
+int
+cons_close(struct Fd *fd)
+{
+	USED(fd);
+
+	return 0;
+}
+
+int
+cons_stat(struct Fd *fd, struct Stat *stat)
+{
+	strcpy(stat->st_name, "<cons>");
+	return 0;
+}
+
---
+++ user/sh.c
@@ -0,0 +1,393 @@
+#include <inc/lib.h>
+
+// TODO:
+// - Opening a file over STDIN_FILENO to run commands from the file
+//   means programs started by sh will have their STDIN_FILENO pointing
+//   to the script file, too. This is ok for starting programs that do no
+//   input, but this approach is wrong for apps that do use stdin.
+
+
+#define BUFSIZ 1024		/* Find the buffer overrun bug! */
+int debug = 0;
+
+
+// gettoken(s, 0) prepares gettoken for subsequent calls and returns 0.
+// gettoken(0, token) parses a shell token from the previously set string,
+// null-terminates that token, stores the token pointer in '*token',
+// and returns a token ID (0, '<', '>', '|', or 'w').
+// Subsequent calls to 'gettoken(0, token)' will return subsequent
+// tokens from the string.
+int gettoken(char* s, char** token);
+
+
+// Parse a shell command from string 's' and execute it.
+// Do not return until the shell command is finished.
+// runcmd() is called in a forked child,
+// so it's OK to manipulate file descriptor state.
+#define MAXARGS 16
+void
+runcmd(char* s)
+{
+	char *argv[MAXARGS], *t, argv0buf[BUFSIZ];
+	int argc, c, i, r, p[2], fd, pipe_child;
+	bool start_in_bkgd;
+
+	pipe_child = 0;
+	gettoken(s, 0);
+	// Find '&' using strchr(), rather than the below case statements,
+	// because the below case statements would only allow the right most
+	// command in a pipe's shell to detect the '&'. Detecting here
+	// allows all commands' shells in a pipe sequence to detect this '&':
+	start_in_bkgd = strchr(s, '&') ? 1 : 0;
+	
+again:
+	argc = 0;
+	while (1) {
+		switch ((c = gettoken(0, &t))) {
+
+		case 'w':	// Add an argument
+			if (argc == MAXARGS) {
+				fprintf(STDERR_FILENO, "too many arguments\n");
+				exit();
+			}
+			argv[argc++] = t;
+			break;
+			
+		case '<':	// Input redirection
+			// Grab the filename from the argument list
+			if (gettoken(0, &t) != 'w') {
+				fprintf(STDERR_FILENO, "syntax error: < not followed by word\n");
+				exit();
+			}
+			// Open 't' for reading as file descriptor 0
+			// (which environments use as standard input).
+			// We can't open a file onto a particular descriptor,
+			// so open the file as 'fd',
+			// then check whether 'fd' is 0.
+			// If not, dup 'fd' onto file descriptor 0,
+			// then close the original 'fd'.
+			
+			fd = open(t, O_RDONLY);
+			if(fd < 0)
+			{
+				fprintf(STDERR_FILENO, "%s: %e\n", t, fd);
+				exit();
+			}
+			if(fd)
+			{
+				dup(fd, 0);
+				close(fd);
+			}
+				
+			break;
+			
+		case '>':	// Output redirection
+			// Grab the filename from the argument list
+			if (gettoken(0, &t) != 'w') {
+				fprintf(STDERR_FILENO, "syntax error: > not followed by word\n");
+				exit();
+			}
+			// Open 't' for writing as file descriptor 1
+			// (which environments use as standard output).
+			// We can't open a file onto a particular descriptor,
+			// so open the file as 'fd',
+			// then check whether 'fd' is 1.
+			// If not, dup 'fd' onto file descriptor 1,
+			// then close the original 'fd'.
+			
+			fd = open(t, O_WRONLY | O_CREAT | O_TRUNC);
+			if(fd < 0)
+			{
+				fprintf(STDERR_FILENO, "%s: %e\n", t, fd);
+				exit();
+			}
+			if(fd != 1)
+			{
+				dup(fd, 1);
+				close(fd);
+			}
+			
+			break;
+			
+		case '|':	// Pipe
+			// Set up pipe redirection.
+			
+			// Allocate a pipe by calling 'pipe(p)'.
+			// Like the Unix version of pipe() (man 2 pipe),
+			// this function allocates two file descriptors;
+			// data written onto 'p[1]' can be read from 'p[0]'.
+			// Then fork.
+			// The child runs the right side of the pipe:
+			//	Use dup() to duplicate the read end of the pipe
+			//	(p[0]) onto file descriptor 0 (standard input).
+			//	Then close the pipe (both p[0] and p[1]).
+			//	(The read end will still be open, as file
+			//	descriptor 0.)
+			//	Then 'goto again', to parse the rest of the
+			//	command line as a new command.
+			// The parent runs the left side of the pipe:
+			//	Set 'pipe_child' to the child env ID.
+			//	dup() the write end of the pipe onto
+			//	file descriptor 1 (standard output).
+			//	Then close the pipe.
+			//	Then 'goto runit', to execute this piece of
+			//	the pipeline.
+
+			r = pipe(p);
+			if(r)
+			{
+				fprintf(STDERR_FILENO, "pipe: %e\n", r);
+				exit();
+			}
+			r = fork();
+			if(r < 0)
+			{
+				fprintf(STDERR_FILENO, "fork: %e\n", r);
+				exit();
+			}
+			if(r)
+			{
+				/* parent */
+				pipe_child = r;
+				dup(p[1], 1);
+				close(p[0]);
+				close(p[1]);
+				goto runit;
+			}
+			/* child */
+			dup(p[0], 0);
+			close(p[0]);
+			close(p[1]);
+			goto again;
+			
+			break;
+
+		case '&':		// Background start
+			// Do nothing here, we detected '&' at the beginning of this fn
+			break;
+
+		case 0:		// String is complete
+			// Run the current command!
+			goto runit;
+			
+		default:
+			panic("bad return %d from gettoken", c);
+			break;
+			
+		}
+	}
+
+runit:
+	// Return immediately if command line was empty.
+	if(argc == 0) {
+		if (debug)
+			printf("EMPTY COMMAND\n");
+		return;
+	}
+
+	// Clean up command line.
+	// Read all commands from the filesystem: add an initial '/' to
+	// the command name.
+	// This essentially acts like 'PATH=/'.
+	if (argv[0][0] == '%') {
+		/* support spawning kernel binaries with % prefix */
+		strcpy(argv0buf, argv[0] + 1);
+		argv[0] = argv0buf;
+	}
+	else if (argv[0][0] != '/') {
+		argv0buf[0] = '/';
+		strcpy(argv0buf + 1, argv[0]);
+		argv[0] = argv0buf;
+	}
+	argv[argc] = 0;
+	
+	// Print the command.
+	if (debug) {
+		printf("[%08x] SPAWN:", env->env_id);
+		for (i = 0; argv[i]; i++)
+			printf(" %s", argv[i]);
+		printf("\n");
+	}
+
+	// Spawn the command!
+	if ((r = spawn(argv[0], (const char **) argv)) < 0)
+		fprintf(STDOUT_FILENO, "spawn %s: %e\n", argv[0], r);
+
+	// In the parent, close all file descriptors and wait for the
+	// spawned command to exit.
+	close_all();
+	if (r >= 0) {
+		if (debug)
+			printf("[%08x] WAIT %s %08x\n", env->env_id, argv[0], r);
+		if (!start_in_bkgd)
+			wait(r);
+		if (debug)
+			printf("[%08x] wait finished\n", env->env_id);
+	}
+
+	// If we were the left-hand part of a pipe,
+	// wait for the right-hand part to finish.
+	if (pipe_child) {
+		if (debug)
+			printf("[%08x] WAIT pipe_child %08x\n", env->env_id, pipe_child);
+		if (!start_in_bkgd)
+			wait(pipe_child);
+		if (debug)
+			printf("[%08x] wait finished\n", env->env_id);
+	}
+
+	// Done!
+	exit();
+}
+
+
+// Get the next token from string s.
+// Set *p1 to the beginning of the token and *p2 just past the token.
+// Returns
+//	0 for end-of-string;
+//	< for <;
+//	> for >;
+//	| for |;
+//	w for a word.
+//
+// Eventually (once we parse the space where the \0 will go),
+// words get nul-terminated.
+#define WHITESPACE " \t\r\n"
+#define SYMBOLS "<|>&;()"
+
+int
+_gettoken(char* s, char** p1, char** p2)
+{
+	int t;
+
+	if (s == 0) {
+		if (debug > 1)
+			printf("GETTOKEN NULL\n");
+		return 0;
+	}
+
+	if (debug > 1)
+		printf("GETTOKEN: %s\n", s);
+
+	*p1 = 0;
+	*p2 = 0;
+
+	while (strchr(WHITESPACE, *s))
+		*s++ = 0;
+	if (*s == 0) {
+		if (debug > 1)
+			printf("EOL\n");
+		return 0;
+	}
+	if (strchr(SYMBOLS, *s)) {
+		t = *s;
+		*p1 = s;
+		*s++ = 0;
+		*p2 = s;
+		if (debug > 1)
+			printf("TOK %c\n", t);
+		return t;
+	}
+	*p1 = s;
+	while (*s && !strchr(WHITESPACE SYMBOLS, *s))
+		s++;
+	*p2 = s;
+	if (debug > 1) {
+		t = **p2;
+		**p2 = 0;
+		printf("WORD: %s\n", *p1);
+		**p2 = t;
+	}
+	return 'w';
+}
+
+int
+gettoken(char* s, char** p1)
+{
+	static int c, nc;
+	static char* np1, *np2;
+
+	if (s) {
+		nc = _gettoken(s, &np1, &np2);
+		return 0;
+	}
+	c = nc;
+	*p1 = np1;
+	nc = _gettoken(np2, &np1, &np2);
+	return c;
+}
+
+
+void
+usage(void)
+{
+	printf("usage: sh [-dix] [command-file]\n");
+	exit();
+}
+
+void
+umain(int argc, char** argv)
+{
+	int r, interactive, echocmds;
+
+	interactive = '?';
+	echocmds = 0;
+	ARGBEGIN{
+	case 'd':
+		debug++;
+		break;
+	case 'i':
+		interactive = 1;
+		break;
+	case 'x':
+		echocmds = 1;
+		break;
+	default:
+		usage();
+	}ARGEND
+
+	if (argc > 1)
+		usage();
+	if (argc == 1) {
+		close(STDIN_FILENO);
+		if ((r = open(argv[0], O_RDONLY)) < 0)
+			panic("open %s: %e", r);
+		assert(r==STDIN_FILENO);
+	}
+	if (interactive == '?')
+		interactive = iscons(0);
+	
+	while (1) {
+		char *buf;
+
+		buf = readline(interactive ? "$ " : NULL);
+		if (buf == NULL) {
+			if (debug)
+				printf("EXITING\n");
+			printf("\n");
+			exit();	// end of file
+		}
+		if (debug)
+			printf("LINE: %s\n", buf);
+		if (buf[0] == '#' || !buf[0])
+			continue;
+		if (echocmds)
+			printf("# %s\n", buf);
+		if (!strcmp(buf, "exit"))
+			exit();
+		if (debug)
+			printf("BEFORE FORK\n");
+		if ((r = fork()) < 0)
+			panic("fork: %e", r);
+		if (debug)
+			printf("FORK: %d\n", r);
+		if (r == 0) {
+			runcmd(buf);
+			exit();
+		} else {
+			if (!strchr(buf, '&'))
+				wait(r);
+		}
+	}
+}
+
