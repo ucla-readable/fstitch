38b3fde8a3cb8e1c2a03a92a41597fd931b8a56f
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2005-02-06 12:19:11 +0000

    r225: This file missed the last commit.

---
+++ kfs/pc_ptable_bd.h
@@ -0,0 +1,9 @@
+#ifndef __KUDOS_KFS_PC_PTABLE_BD_H
+#define __KUDOS_KFS_PC_PTABLE_BD_H
+
+#include <inc/types.h>
+#include <kfs/bd.h>
+
+BD_t * pc_ptable_bd(BD_t * disk, uint8_t partition);
+
+#endif /* __KUDOS_KFS_PC_PTABLE_BD_H */
