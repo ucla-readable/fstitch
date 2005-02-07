4d3d553db3d97ccbb36006e2044aec892c7bdd2c
Chris Frost <frost@cs.ucla.edu>
2005-02-07 06:35:32 +0000

    r230: - Initial version of serialized CFS methods

---
+++ inc/serial_cfs.h
@@ -0,0 +1,123 @@
+#ifndef __KUDOS_INC_SERIAL_CFS_H
+#define __KUDOS_INC_SERIAL_CFS_H
+
+#include <inc/types.h>
+
+//
+// CFS Methods
+
+#define SCFS_OPEN 1
+#define SCFS_CLOSE 2
+#define SCFS_READ 3
+#define SCFS_WRITE 4
+#define SCFS_TRUNCATE 5
+#define SCFS_UNLINK 6
+#define SCFS_LINK 7
+#define SCFS_RENAME 8
+#define SCFS_MKDIR 9
+#define SCFS_RMDIR 10
+#define SCFS_GET_FEATURES 11
+#define SCFS_GET_METADATA 12
+#define SCFS_SET_METADATA 13
+#define SCFS_SYNC 13
+
+#define SCFS_TYPE int scfs_type
+
+// MAXPATHLEN is the maxiumum length we can fit in a method page, given
+// where names are used and the common denonimator amount of space available
+// in these pages.
+#define SCFSMAXNAMELEN ((PGSIZE - 2*sizeof(int)) / 2)
+
+
+struct Scfs_open {
+	SCFS_TYPE;
+	int mode;
+	char path[SCFSMAXNAMELEN];
+};
+
+struct Scfs_close {
+	SCFS_TYPE;
+	int fid;
+};
+
+struct Scfs_read {
+	SCFS_TYPE;
+	int fid;
+	uint32_t offset;
+	uint32_t size;
+};
+
+struct Scfs_write {
+	SCFS_TYPE;
+	int fid;
+	uint32_t offset;
+	uint32_t size;
+	// data is sent as a separate page
+};
+
+struct Scfs_truncate {
+	SCFS_TYPE;
+	int fid;
+	uint32_t size;
+};
+
+struct Scfs_unlink {
+	SCFS_TYPE;
+	char name[SCFSMAXNAMELEN];
+};
+
+struct Scfs_link {
+	SCFS_TYPE;
+	char oldname[SCFSMAXNAMELEN];
+	char newname[SCFSMAXNAMELEN];
+};
+
+struct Scfs_rename {
+	SCFS_TYPE;
+	char oldname[SCFSMAXNAMELEN];
+	char newname[SCFSMAXNAMELEN];
+};
+
+struct Scfs_mkdir {
+	SCFS_TYPE;
+	char path[SCFSMAXNAMELEN];
+};
+
+struct Scfs_rmdir {
+	SCFS_TYPE;
+	char path[SCFSMAXNAMELEN];
+};
+
+struct Scfs_get_features {
+	SCFS_TYPE;
+	char name[SCFSMAXNAMELEN];
+};
+
+struct Scfs_get_metadata {
+	SCFS_TYPE;
+	char name[SCFSMAXNAMELEN];
+	
+};
+
+struct Scfs_set_metadata {
+	SCFS_TYPE;
+	char name[SCFSMAXNAMELEN];
+	// Scfs_metadata is sent as a separate page
+};
+
+struct Scfs_sync {
+	SCFS_TYPE;
+	int fid;
+};
+
+
+//
+// CFS "data-page-blobs"
+
+struct Scfs_metdata {
+	int type;
+	uint32_t size;
+	uint8_t data[PGSIZE - sizeof(int) - sizeof(uint32_t)];
+};
+
+#endif // not __KUDOS_INC_SERIAL_CFS_H
