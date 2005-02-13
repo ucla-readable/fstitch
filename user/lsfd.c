aabe5415c25e55a8a7b94a7cd3613327b3fa9af4
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2005-02-13 12:51:35 +0000

    r333: Updating lsfd to be able to use stderr as well as stdout.

---
+++ user/lsfd.c
@@ -0,0 +1,38 @@
+#include <inc/lib.h>
+
+void
+usage(void)
+{
+	printf("usage: lsfd [-1|-2]\n");
+	exit();
+}
+
+void
+umain(int argc, char **argv)
+{
+	int i, usefd = -1;
+	struct Stat st;
+
+	ARGBEGIN{
+	default:
+		usage();
+	case '1':
+		usefd = 1;
+		break;
+	case '2':
+		usefd = 2;
+		break;
+	}ARGEND
+
+	for (i=0; i<32; i++)
+		if (fstat(i, &st) >= 0) {
+			if (usefd != -1)
+				fprintf(usefd, "fd %d: name %s isdir %d size %d dev %s\n",
+					i, st.st_name, st.st_isdir,
+					st.st_size, st.st_dev->dev_name);	
+			else
+				printf_c("fd %d: name %s isdir %d size %d dev %s\n",
+					i, st.st_name, st.st_isdir,
+					st.st_size, st.st_dev->dev_name);
+		}
+}
