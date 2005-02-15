b98b9272e29777193c01c943121900b0923fde23
Chris Frost <frost@cs.ucla.edu>
2005-02-15 10:35:08 +0000

    r365: - Added dos classifier header.

---
+++ kfs/dos_classifier.h
@@ -0,0 +1,8 @@
+#ifndef __KUDOS_KFS_DOS_CLASS_H
+#define __KUDOS_KFS_DOS_CLASS_H
+
+#include <kfs/cfs.h>
+
+CFS_t * dos_classifier(CFS_t * cfs1, const char * p1, CFS_t * cfs2, const char * p2);
+
+#endif // not __KUDOS_KFS_DOS_CLASS_H
