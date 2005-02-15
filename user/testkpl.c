467ec8bbce71b054ce7f1017b1f857c9ece0ad07
Chris Frost <frost@cs.ucla.edu>
2005-02-15 10:31:51 +0000

    r364: - Updated to read, write, then read twice from a given file and write   using the given text.

--- user/testkpl.c
+++ user/testkpl.c
@@ -1,24 +1,52 @@
 #include <inc/lib.h>
 #include <inc/kpl.h>
 
-char buf[8192];
-
+char data[2*PGSIZE];
 
 void
 umain(int argc, char **argv)
 {
+	uint32_t offset = 513;
 	int r;
-	char data[11];
 	int fd;
 
-	fd = kpl_open("/temp", 0);
-	printf("f = kpl_open(\"/temp\", 0); = %d\n", fd);
-	r = read(fd, data, sizeof(data)-1);
-	printf("r = read(f, 0, 10, data); = %d\n", r);
-	data[sizeof(data)-1] = '\0';
-	r = close(fd);
-	printf("r = close(f); = %d %e\n", r);
+	assert(argc == 3);
+	char * filename = argv[1];
+	char * write_data = argv[2];
 
+	fd = kpl_open(filename, O_RDWR);
+	printf("f = kpl_open(\"/%s\", 0); = %e\n", filename, fd);
+
+	r = read(fd, data, offset);
+	if (r < 0)
+		printf("r = read(f, , 0x%x, data); = %e\n", r);
 	printf("data: [%s]\n", data);
+
+	r = seek(fd, 0);
+	printf("r = seek(f, 0); = %e\n", r);
+
+	r = write(fd, write_data, strlen(write_data));
+	printf("r = write(f, \"%s\", 0x%x); = %e\n", write_data, strlen(write_data), r);
+
+	r = seek(fd, 0);
+	printf("r = seek(f, 0); = %e\n", r);
+
+	r = read(fd, data, offset);
+	if (r < 0)
+		printf("r = read(f, , 0x%x, data); = %e\n", r);
+	printf("data: [%s]\n", data);
+
+	r = read(fd, data, offset);
+	printf("r = read(f, , 0x%x, data); = %e\n", offset, r);
+	data[offset] = '\0';
+	printf("data: [%s]\n", data);
+
+	r = read(fd, data, offset);
+	printf("r = read(f, , 0x%x, data); = %e\n", offset, r);
+	data[offset] = '\0';
+	printf("data: [%s]\n", data);
+
+	r = close(fd);
+	printf("r = close(f); = %e\n", r);
 }
 
