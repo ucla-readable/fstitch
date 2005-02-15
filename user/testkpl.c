78f65b33d6945275848a376d3de7b4945ef22542
Chris Frost <frost@cs.ucla.edu>
2005-02-15 10:40:53 +0000

    r367: - Now simplified and improved!

--- user/testkpl.c
+++ user/testkpl.c
@@ -10,43 +10,40 @@ umain(int argc, char **argv)
 	int r;
 	int fd;
 
-	assert(argc == 3);
+	if (argc != 3)
+	{
+		fprintf(STDERR_FILENO, "Usage: %s <path> <text_to_write>\n", argv[0]);
+		exit();
+	}
+
 	char * filename = argv[1];
 	char * write_data = argv[2];
 
 	fd = kpl_open(filename, O_RDWR);
-	printf("f = kpl_open(\"/%s\", 0); = %e\n", filename, fd);
+	printf("kpl_open(\"/%s\", 0) = %e\n", filename, fd);
 
 	r = read(fd, data, offset);
-	if (r < 0)
-		printf("r = read(f, , 0x%x, data); = %e\n", r);
+	printf("read(fd, data, 0x%x) = %e\n", offset, r);
 	printf("data: [%s]\n", data);
 
 	r = seek(fd, 0);
-	printf("r = seek(f, 0); = %e\n", r);
+	printf("seek(fd, 0) = %e\n", r);
 
 	r = write(fd, write_data, strlen(write_data));
-	printf("r = write(f, \"%s\", 0x%x); = %e\n", write_data, strlen(write_data), r);
+	printf("write(fd, \"%s\", 0x%x) = %e\n", write_data, strlen(write_data), r);
 
 	r = seek(fd, 0);
-	printf("r = seek(f, 0); = %e\n", r);
+	printf("seek(fd, 0) = %e\n", r);
 
 	r = read(fd, data, offset);
-	if (r < 0)
-		printf("r = read(f, , 0x%x, data); = %e\n", r);
+	printf("read(fd, data, 0x%x) = %e\n", offset, r);
 	printf("data: [%s]\n", data);
 
 	r = read(fd, data, offset);
-	printf("r = read(f, , 0x%x, data); = %e\n", offset, r);
-	data[offset] = '\0';
-	printf("data: [%s]\n", data);
-
-	r = read(fd, data, offset);
-	printf("r = read(f, , 0x%x, data); = %e\n", offset, r);
-	data[offset] = '\0';
+	printf("read(fd, data, 0x%x) = %e\n", offset, r);
 	printf("data: [%s]\n", data);
 
 	r = close(fd);
-	printf("r = close(f); = %e\n", r);
+	printf("close(fd) = %e\n", r);
 }
 
