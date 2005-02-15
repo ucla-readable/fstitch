0f080d80e7405275f441bcd03bf72aa45b1dbb39
Andrew de los Reyes <andrew-featherstitchgit@gizmolabs.org>
2005-02-15 06:07:56 +0000

    r352: test kpl

---
+++ user/testkpl.c
@@ -0,0 +1,24 @@
+#include <inc/lib.h>
+#include <inc/kpl.h>
+
+char buf[8192];
+
+
+void
+umain(int argc, char **argv)
+{
+	int r;
+	char data[11];
+	int fd;
+
+	fd = kpl_open("/temp", 0);
+	printf("f = kpl_open(\"/temp\", 0); = %d\n", fd);
+	r = read(fd, data, sizeof(data)-1);
+	printf("r = read(f, 0, 10, data); = %d\n", r);
+	data[sizeof(data)-1] = '\0';
+	r = close(fd);
+	printf("r = close(f); = %d\n", r);
+
+	printf("data: [%s]\n", data);
+}
+
