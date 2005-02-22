ad1549889a460204324814111329d5af135f7518
Chris Frost <frost@cs.ucla.edu>
2005-02-22 08:39:17 +0000

    r484: - Updated to assert that the file just created is a dir to use the correct   interface (fstat instead of fd_lookup() ...).

---
+++ user/mkdir.c
@@ -0,0 +1,35 @@
+#include <inc/lib.h>
+
+void
+print_usage(char *bin)
+{
+	printf("%s: <dir> [<dir> ...]\n", bin);
+}
+
+void
+umain(int argc, char **argv)
+{
+	if(argc < 2)
+	{
+		print_usage(argv[0]);
+		exit();
+	}
+
+	int i;
+	for(i=1; i<argc; i++)
+	{
+		int r;
+		int fd;
+		if((fd = r = open(argv[i], O_CREAT | O_MKDIR)) < 0)
+		{
+			fprintf(STDERR_FILENO, "open(%s): %e\n", argv[i], r);
+			exit();
+		}
+
+		// Ensure the directory was created, in case of buggy fs
+		struct Stat s;
+		if((r = fstat(fd, &s)) < 0)
+			panic("fstat: %e", r);
+		assert(s.st_isdir);
+	}
+}
