6420d03d15e8614d70ecf48b2e6d7b17225b91d6
Chris Frost <frost@cs.ucla.edu>
2005-02-25 10:43:43 +0000

    r522: - Comment updates.

--- kfs/fidfairy_cfs.h
+++ kfs/fidfairy_cfs.h
@@ -1,15 +1,15 @@
 #ifndef __KUDOS_KFS_FIDFAIRY_CFS_H
 #define __KUDOS_KFS_FIDFAIRY_CFS_H
 
-// fidfairy_cfs notices when fids are no longer in use, by analyzing
-// the pageref count associated with the fid, and calls close on
-// fidfairy's frontend_cfs exactly when it has detected a fid is no
-// longer in use.
+// fidfairy_cfs helps out its frontend_cfs by determining when fids
+// are no longer in use, by analyzing the pageref count associated
+// with the fid, and calling close on fidfairy's frontend_cfs exactly
+// when fidfairy has detected a fid is no longer in use.
 
 // Characterization of fidfairy:
 // depman is characterized as Santa Claus. fidfairy, as a fairy god mother.
 // Modules don't know of fidfairy, but fidfairy is there behind the scenes
-// helping CFS modules out by letting them know when fids are no longer in use.
+// helping CFS modules out.
 
 
 #include <kfs/cfs.h>
