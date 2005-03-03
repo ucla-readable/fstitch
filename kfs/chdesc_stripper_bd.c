5a6cf1682804f319e091134a764af73e0401f523
Chris Frost <frost@cs.ucla.edu>
2005-03-03 11:41:20 +0000

    r572: - Created chdesc_stripper_bd. - Enhanced kfsd_init to auto detect more and made it a bit easier to add   more BDs.

---
+++ kfs/chdesc_stripper_bd.c
@@ -0,0 +1,208 @@
+#include <inc/malloc.h>
+
+#include <kfs/chdesc.h>
+#include <kfs/depman.h>
+#include <kfs/chdesc_stripper_bd.h>
+
+
+#define CHDESC_STRIPPER_DEBUG 1
+
+#if CHDESC_STRIPPER_DEBUG
+#define Dprintf(x...) printf(x)
+#else
+#define Dprintf(x...)
+#endif
+
+
+struct chdesc_stripper_state {
+	BD_t * bd;
+};
+typedef struct chdesc_stripper_state chdesc_stripper_state_t;
+
+
+//
+// Intercepted BD_t functions
+
+static int chdesc_stripper_write_block(BD_t * bd, bdesc_t * block)
+{
+	Dprintf("%s(0x%08x)\n", __FUNCTION__, block);
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+	const chdesc_t * tmp;
+	chdesc_t * block_chdesc, * cur_chdesc;
+	uint32_t refs;
+	int r;
+
+	if (block->bd != bd)
+		printf("block->bd 0x%08x, bd 0x%08x\n", block->bd, bd);
+	assert(block->bd == bd);
+
+	tmp = depman_get_deps(block);
+	r = chdesc_weak_retain((chdesc_t *) tmp, &block_chdesc);
+	if (r < 0)
+		return r;
+
+	assert(!block_chdesc || !block_chdesc->dependents); // no one should depend on block
+
+	// Satisfy inter-BD deps and remove intra-BD deps
+
+	while (block_chdesc && block_chdesc->dependencies
+		   && (cur_chdesc = block_chdesc->dependencies->desc))
+	{
+		if (cur_chdesc->block->bd == bd)
+		{
+			r = depman_remove_chdesc(cur_chdesc);
+			assert(r >= 0);
+		}
+		else
+		{
+			r = CALL(cur_chdesc->block->bd, sync, cur_chdesc->block);
+			if (r < 0)
+			{
+				fprintf(STDERR_FILENO, "%s: while stripping, a BD write errored: %e\n", __FUNCTION__, r);
+				return r;
+			}
+		}
+	}
+
+	if (block_chdesc)
+	{
+		r = depman_remove_chdesc(block_chdesc);
+		assert(r >= 0);
+	}
+
+	// Write the block
+
+	refs = block->refs;
+	block->translated++;
+	block->bd = state->bd;
+
+	r = CALL(state->bd, write_block, block);
+
+	if (refs)
+	{
+		block->bd = bd;
+		block->translated--;
+	}
+
+	return r;
+}
+
+static int chdesc_stripper_destroy(BD_t * bd)
+{
+	Dprintf("%s(0x%08x)\n", __FUNCTION__, bd);
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+
+	memset(state, 0, sizeof(*state));
+	free(state);
+	memset(bd, 0, sizeof(*bd));
+	free(bd);
+
+	return 0;
+}
+
+
+//
+// Passthrough BD_t functions needing translation
+
+static bdesc_t * chdesc_stripper_read_block(BD_t * bd, uint32_t number)
+{
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+	bdesc_t * bdesc;
+	int r;
+
+	if (!(bdesc = CALL(state->bd, read_block, number)))
+		return NULL;
+
+	// adjust bdesc to match this bd
+	if ((r = bdesc_alter(&bdesc)) < 0)
+	{
+		bdesc_drop(&bdesc);
+		return NULL;
+	}
+	bdesc->bd = bd;
+
+	return bdesc;
+}
+
+static int chdesc_stripper_sync(BD_t * bd, bdesc_t * block)
+{
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+	uint32_t refs;
+	int r;
+
+	if (!block)
+		return CALL(state->bd, sync, NULL);
+
+	assert(block->bd == bd);
+
+	refs = block->refs;
+	block->translated++;
+	block->bd = state->bd;
+
+	r = CALL(state->bd, sync, block);
+
+	if (refs)
+	{
+		block->bd = bd;
+		block->translated--;
+	}
+
+	return r;
+}
+
+
+//
+// Passthrough BD_t functions
+
+static uint32_t chdesc_stripper_get_numblocks(BD_t * bd)
+{
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+	return CALL(state->bd, get_numblocks);
+}
+
+static uint16_t chdesc_stripper_get_blocksize(BD_t * bd)
+{
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+	return CALL(state->bd, get_blocksize);
+}
+
+static uint16_t chdesc_stripper_get_atomicsize(BD_t * bd)
+{
+	chdesc_stripper_state_t * state = (chdesc_stripper_state_t *) bd->instance;
+	return CALL(state->bd, get_atomicsize);
+}
+
+
+//
+//
+
+BD_t * chdesc_stripper_bd(BD_t * disk)
+{
+	BD_t * bd;
+	chdesc_stripper_state_t * state;
+
+	bd = malloc(sizeof(*bd));
+	if (!bd)
+		return NULL;
+	
+	state = malloc(sizeof(*state));
+	if (!state)
+		goto error_bd;
+	bd->instance = state;
+	
+	ASSIGN(bd, chdesc_stripper, get_numblocks);
+	ASSIGN(bd, chdesc_stripper, get_blocksize);
+	ASSIGN(bd, chdesc_stripper, get_atomicsize);
+	ASSIGN(bd, chdesc_stripper, read_block);
+	ASSIGN(bd, chdesc_stripper, write_block);
+	ASSIGN(bd, chdesc_stripper, sync);
+	ASSIGN_DESTROY(bd, chdesc_stripper, destroy);
+
+	state->bd = disk;
+	
+	return bd;
+
+  error_bd:
+	free(bd);
+	return NULL;
+}
--- kfs/kfsd_init.c
+++ kfs/kfsd_init.c
@@ -1,8 +1,10 @@
+#include <inc/vector.h>
 #include <inc/lib.h>
 
 #include <kfs/ide_pio_bd.h>
 #include <inc/partition.h>
 #include <kfs/pc_ptable_bd.h>
+#include <kfs/chdesc_stripper_bd.h>
 #include <kfs/wt_cache_bd.h>
 #include <kfs/block_resizer_bd.h>
 #include <kfs/nbd_bd.h>
@@ -19,81 +21,188 @@
 #include <kfs/kfsd.h>
 #include <kfs/kfsd_init.h>
 
-static const char * josfspath = "/";
-static const char * fspaths[] = {"/k0", "/k1", "/k2", "/k4"};
+int construct_uhfses(BD_t * bd, uint32_t cache_nblks, vector_t * uhfses);
+
+
+static const char * fspaths[] = {"/", "/k0", "/k1", "/k2", "/k4"};
 
 // Init kfsd modules.
 int kfsd_init(void)
 {
-	BD_t * bd_disk = NULL;
-	bool use_disk_1 = 0, use_net = 0;
-	void * ptbl = NULL;
-	BD_t * partitions[4] = {NULL};
-	CFS_t * uhfses[2] = {NULL};
+	const bool use_disk_0 = 1;
+	const bool use_disk_1 = 0;
+	const bool use_net    = 0;
+
+	vector_t * uhfses = NULL;
 	CFS_t * josfscfs = NULL;
 	CFS_t * table_class = NULL;
 	CFS_t * fidprotector = NULL;
 	CFS_t * fidfairy = NULL;
-	uint32_t i;
 	int r;
 
 	if((r = depman_init()) < 0)
 	{
-		printf("Failed to initialize DEP MAN: %e\n", r);
-		return r;
+		fprintf(STDERR_FILENO, "depman_init: %e\n", r);
+		kfsd_shutdown();
 	}
 
 	if (!cfs_ipc_serve_init())
+	{
+		fprintf(STDERR_FILENO, "cfs_ipc_serve_init failed\n");
 		kfsd_shutdown();
+	}
 
-	if (sched_init() < 0)
+	if ((r = sched_init()) < 0)
+	{
+		fprintf(STDERR_FILENO, "sched_init: %e\n", r);
 		kfsd_shutdown();
+	}
 
+	//
+	// Setup uhfses
+
+	if (! (uhfses = vector_create()) )
+	{
+		fprintf(STDERR_FILENO, "OOM, vector_create\n");
+		kfsd_shutdown();
+	}
+
+	if (use_net)
+	{
+		BD_t * bd;
+
+		/* delay kfsd startup slightly for netd to start */
+		sleep(200);
+
+		if (! (bd = nbd_bd("192.168.2.1", 2492)) )
+		{
+			fprintf(STDERR_FILENO, "nbd_bd failed\n");
+			kfsd_shutdown();
+		}
+
+		if ((r = construct_uhfses(bd, 400, uhfses)) < 0)
+			kfsd_shutdown();
+	}
+
+	if (use_disk_0)
+	{
+		BD_t * bd;
+
+		if (! (bd = ide_pio_bd(0)) )
+		{
+			fprintf(STDERR_FILENO, "ide_pio_bd(0) failed\n");
+			kfsd_shutdown();
+		}
+
+		if ((r = construct_uhfses(bd, 32, uhfses)) < 0)
+			kfsd_shutdown();
+	}
 
 	if (use_disk_1)
 	{
-		if (! (bd_disk = ide_pio_bd(1)) )
+		BD_t * bd;
+
+		if (! (bd = ide_pio_bd(1)) )
+		{
+			fprintf(STDERR_FILENO, "ide_pio_bd(1) failed\n");
 			kfsd_shutdown();
+		}
 
-		/* discover partitions */
-		ptbl = pc_ptable_init(bd_disk);
-		if (ptbl)
+		if ((r = construct_uhfses(bd, 32, uhfses)) < 0)
+			kfsd_shutdown();
+	}
+
+	//
+	// Mount uhfses
+
+	if (! (table_class = table_classifier_cfs(NULL, NULL, 0)) )
+		kfsd_shutdown();
+	assert(!get_frontend_cfs());
+	set_frontend_cfs(table_class);
+
+	{
+		const size_t uhfses_size = vector_size(uhfses);
+		size_t i;
+		for (i=0; i < uhfses_size; i++)
 		{
-			uint32_t max = pc_ptable_count(ptbl);
-			printf("Found %d partitions.\n", max);
-			for (i = 1; i <= max; i++)
-			{
-				uint8_t type = pc_ptable_type(ptbl, i);
-				printf("Partition %d has type %02x\n", i, type);
-				if (type == PTABLE_KUDOS_TYPE)
-					partitions[i-1] = pc_ptable_bd(ptbl, i);
-			}
-			pc_ptable_free(ptbl);
-			
-			if (!partitions[0] && !partitions[1] && !partitions[2] && !partitions[3])
-			{
-				printf("No KudOS partition found!\n");
+			r = table_classifier_cfs_add(table_class, fspaths[i], vector_elt(uhfses, i));
+			if (r < 0)
 				kfsd_shutdown();
-			}
-		}
-		else
-		{
-			printf("Using whole disk.\n");
-			partitions[0] = bd_disk;
 		}
 
-		/* setup each partition's cache, basefs, and uhfs */
-		for (i=0; i < 4; i++)
+		vector_destroy(uhfses);
+		uhfses = NULL;
+	}
+
+	//
+	// fidfairies
+
+	if (! (fidprotector = fidprotector_cfs(get_frontend_cfs())) )
+		kfsd_shutdown();
+	// Uncomment when cfs_ipc_client supports capabilities:
+	//set_frontend_cfs(fidprotector);
+
+	if (! (fidfairy = fidfairy_cfs(get_frontend_cfs())) )
+		kfsd_shutdown();
+	set_frontend_cfs(fidfairy);
+
+	return 0;
+}
+
+
+// Bring up the filesystems for bd and add them to uhfses.
+int construct_uhfses(BD_t * bd, uint32_t cache_nblks, vector_t * uhfses)
+{
+	void * ptbl = NULL;
+	BD_t * partitions[4] = {NULL};
+	uint32_t i;
+
+	/* discover partitions */
+	ptbl = pc_ptable_init(bd);
+	if (ptbl)
+	{
+		uint32_t max = pc_ptable_count(ptbl);
+		printf("Found %d partitions.\n", max);
+		for (i = 1; i <= max; i++)
 		{
-			BD_t * cache;
-			BD_t * resizer;
-			LFS_t * lfs;
+			uint8_t type = pc_ptable_type(ptbl, i);
+			printf("Partition %d has type %02x\n", i, type);
+			if (type == PTABLE_KUDOS_TYPE)
+				partitions[i-1] = pc_ptable_bd(ptbl, i);
+		}
+		pc_ptable_free(ptbl);
 			
-			if (!partitions[i])
-				continue;
+		if (!partitions[0] && !partitions[1] && !partitions[2] && !partitions[3])
+		{
+			printf("No KudOS partition found!\n");
+			//kfsd_shutdown();
+		}
+	}
+	else
+	{
+		printf("Using whole disk.\n");
+		partitions[0] = bd;
+	}
 
+	// HACK
+	if (!partitions[0])
+		partitions[0] = bd;
+
+	/* setup each partition's cache, basefs, and uhfs */
+	for (i=0; i < 4; i++)
+	{
+		BD_t * cache;
+		BD_t * resizer;
+		LFS_t * lfs;
+		CFS_t * u;
+			
+		if (!partitions[i])
+			continue;
+
+		if (4096 != CALL(partitions[i], get_atomicsize))
+		{
 			/* create a cache below the resizer */
-			if (! (cache = wt_cache_bd(partitions[i], 32)) )
+			if (! (cache = wt_cache_bd(partitions[i], cache_nblks)) )
 				kfsd_shutdown();
 
 			/* create a resizer */
@@ -104,90 +213,32 @@ int kfsd_init(void)
 			if (! (cache = wt_cache_bd(resizer, 4)) )
 				kfsd_shutdown();
 
-			/* make josfs on partition 2 (index 1), else wholedisk */
-			if (i == 1)
-			{
-				if (! (lfs = josfs(cache)) )
-					kfsd_shutdown();
-			}
-			else
-			{
-				if (! (lfs = wholedisk(cache)) )
-					kfsd_shutdown();
-			}
-
-			if (! (uhfses[i] = uhfs(lfs)) )
+			//if (! (cache = chdesc_stripper_bd(cache)) )
+			//	kfsd_shutdown();
+		}
+		else
+		{
+			if (! (cache = wt_cache_bd(partitions[i], cache_nblks)) )
 				kfsd_shutdown();
 		}
-	}
 
-	// table_classifier_cfs
-	if (! (table_class = table_classifier_cfs(NULL, NULL, 0)) )
-		kfsd_shutdown();
-	assert(!get_frontend_cfs());
-	set_frontend_cfs(table_class);
+		if ((lfs = josfs(cache)))
+			printf("Using josfs");
+		else if ((lfs = wholedisk(cache)))
+			printf("Using wholedisk");
+		else
+			kfsd_shutdown();
 
-	// josfs_cfs
-	if (! (josfscfs = josfs_cfs()) )
-		kfsd_shutdown();
-	r = table_classifier_cfs_add(table_class, josfspath, josfscfs);
-	if (r < 0)
-		kfsd_shutdown();
+		if (i == 0 && partitions[0] == bd)
+			printf(" on disk.\n");
+		else
+			printf(" on partition %d.\n", i);
 
-	// Mount the uhfs modules
-	for (i=0; i < sizeof(uhfses)/sizeof(uhfses[0]); i++)
-	{
-		if (!uhfses[i])
-			continue;
-
-		r = table_classifier_cfs_add(table_class, fspaths[i], uhfses[i]);
-		if (r < 0)
+		if (! (u = uhfs(lfs)) )
+			kfsd_shutdown();
+		if (vector_push_back(uhfses, u) < 0)
 			kfsd_shutdown();
 	}
-	
-	if(use_net)
-	{
-		BD_t * nbd;
-		BD_t * cache;
-		LFS_t * fs;
-		CFS_t * net;
-
-		/* delay kfsd startup slightly for netd to start */
-		sleep(200);
-
-		nbd = nbd_bd("192.168.4.15", 2492);
-		if(!nbd)
-			goto a;
-		cache = wt_cache_bd(nbd, 192);
-		if(!cache)
-			goto b;
-		fs = josfs(cache);
-		if(!fs)
-			goto c;
-		net = uhfs(fs);
-		if(!net)
-			goto d;
-
-		if(table_classifier_cfs_add(table_class, "/net", net))
-		{
-			DESTROY(net);
-			d: DESTROY(fs);
-			c: DESTROY(cache);
-			b: DESTROY(nbd);
-			a: (void) 0;
-		}
-	}
-
-	// fidprotector
-	if (! (fidprotector = fidprotector_cfs(get_frontend_cfs())) )
-		kfsd_shutdown();
-	// Uncomment when cfs_ipc_client supports capabilities:
-	//set_frontend_cfs(fidprotector);
-
-	// fidfairy
-	if (! (fidfairy = fidfairy_cfs(get_frontend_cfs())) )
-		kfsd_shutdown();
-	set_frontend_cfs(fidfairy);
 
 	return 0;
 }
