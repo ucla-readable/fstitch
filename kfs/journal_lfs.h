0bf6ac0ea2a1132284e8b976548cc33132e95b1a
Chris Frost <frost@cs.ucla.edu>
2005-03-05 08:57:19 +0000

    r621: - Updated journal_lfs()'s parameter names.

--- kfs/journal_lfs.h
+++ kfs/journal_lfs.h
@@ -4,6 +4,6 @@
 #include <kfs/bd.h>
 #include <kfs/lfs.h>
 
-LFS_t * journal_lfs(LFS_t * journal_bd, LFS_t * fs_bd, BD_t * fs_queue);
+LFS_t * journal_lfs(LFS_t * journal_lfs, LFS_t * fs_lfs, BD_t * fs_queue);
 
 #endif /* __KUDOS_KFS_JOURNAL_LFS_H */
