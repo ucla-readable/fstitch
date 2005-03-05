4a07e63a8344c04576f9ad3879c69acf7e93ee72
Chris Frost <frost@cs.ucla.edu>
2005-03-05 09:28:41 +0000

    r623: - Improved parameter naming, again.

--- kfs/journal_lfs.h
+++ kfs/journal_lfs.h
@@ -4,6 +4,6 @@
 #include <kfs/bd.h>
 #include <kfs/lfs.h>
 
-LFS_t * journal_lfs(LFS_t * journal_lfs, LFS_t * fs_lfs, BD_t * fs_queue);
+LFS_t * journal_lfs(LFS_t * journal, LFS_t * fs, BD_t * fs_queue);
 
 #endif /* __KUDOS_KFS_JOURNAL_LFS_H */
