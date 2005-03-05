849df07589149e4021448d08fe53ba1a3dc84efe
Andrew de los Reyes <andrew-featherstitchgit@gizmolabs.org>
2005-03-05 12:32:54 +0000

    r626: clearer explanation of qsort()

--- inc/lib.h
+++ inc/lib.h
@@ -183,6 +183,9 @@ int hwclock_time(int *t);
 int bcd2dec(int bcd);
 
 // qsort.c
+// Sort in ascending order. compar should return a value less than,
+// equal to, or greater than zero if 'a' is less than, equal to, or
+// greater than 'b', respectively.
 void qsort(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 
 /* File open modes */
