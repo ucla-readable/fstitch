61a37486eb099b66f332e1722aa6b251388cb3bf
Chris Frost <frost@cs.ucla.edu>
2005-03-11 04:14:26 +0000

    r749: Beginning of kfsd rpc. - Split cfs_ipc_serve's functionality into ipc_serve and cfs_ipc_serve - Added kfs_ipc_serve - Added kfs_ipc_client - Added oo.h:DESTROY_LOCAL macro for non-kfsd envs to use to destroy their   local interface to a kfsd object   [This checkin was missed in the previous three.]

---
+++ kfs/ipc_serve.h
@@ -0,0 +1,13 @@
+#ifndef __KUDOS_KFS_IPC_SERVE_H
+#define __KUDOS_KFS_IPC_SERVE_H
+
+#include <kfs/fidcloser_cfs.h>
+
+// VA at which to receive page mappings containing client reqs.
+// Just before the range used by the UHFS module for mapping client pages.
+#define IPCSERVE_REQVA (FIDCLOSER_CFS_FD_MAP - PGSIZE)
+
+int  ipc_serve_init(void);
+void ipc_serve_run(void);
+
+#endif // __KUDOS_KFS_IPC_SERVE_H
