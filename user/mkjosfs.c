ed5b39f7db912688b8939a54b25f07dae87fc5ba
Andrew de los Reyes <andrew-featherstitchgit@gizmolabs.org>
2005-03-13 22:39:11 +0000

    r793: Make a josfs from within kudos. This program does not allow you to specify the ideal filesystem size. It just stat()s the file/device you give it and initializes the beginning to contain a valid superblock. Thus, to create a new loopback filesystem, one would need to first create a large file, then run mkjosfs on that file. This utility is not yet tested.

---
+++ user/mkjosfs.c
@@ -0,0 +1,62 @@
+#include <inc/lib.h>
+#include <inc/fs.h>
+
+void usage(char *);
+
+void
+umain(int argc, char *argv[])
+{
+	struct Super s;
+	struct File *f;
+	struct Stat st;
+	int flen;
+	int r;
+	int fd;
+	int i;
+
+	if (argc != 2) usage(argv[0]);
+	fd = open(argv[1], O_WRONLY);
+	if (fd < 0) {
+		fprintf(STDERR_FILENO, "Unable to open %s\n", argv[1]);
+		exit();
+	}
+	r = fstat(fd, &st);
+	if (r < 0) {
+		fprintf(STDERR_FILENO, "Unable to stat %s\n", argv[1]);
+		exit();
+	}
+	if (st.st_isdir) {
+		fprintf(STDERR_FILENO, "Error: %s is a directory\n", argv[1]);
+		exit();
+	}
+
+	flen = st.st_size;
+	
+	s.s_magic = FS_MAGIC;
+	s.s_nblocks = flen/4096;
+
+	f = &s.s_root;
+
+	strcpy(f->f_name, "/");
+	f->f_size = 0;
+	f->f_type = FTYPE_DIR;
+	for (i = 0; i < NDIRECT; i++)
+		f->f_direct[i] = 0;
+	f->f_indirect = 0;
+	f->f_dir = 0;
+
+	r = write(fd, &s, sizeof(s));
+	if (r < sizeof(s)) {
+		fprintf(STDERR_FILENO, "Error: Only wrote %d bytes to %s, needed %d\n",
+				r, argv[1], sizeof(s));
+		exit();
+	}
+	close(fd);
+	printf("Success. New filesystem has %d blocks.\n", s.s_nblocks);
+}
+
+void
+usage(char *s)
+{
+	printf("usage: %s [existing filename|devicename]");
+}
