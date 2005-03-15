80950cb19b8055e298dce054879a25cb8a7714f0
Andrew de los Reyes <andrew-featherstitchgit@gizmolabs.org>
2005-03-15 12:22:37 +0000

    r802: latest version of the wb cache for consumption!

--- kfs/wb_cache_bd.c
+++ kfs/wb_cache_bd.c
@@ -14,12 +14,64 @@
 #include <kfs/modman.h>
 #include <kfs/wb_cache_bd.h>
 
+#define WB_CACHE_DEBUG 0
+#define WB_CACHE_DEBUG_TREE 0
+
+#if WB_CACHE_DEBUG
+#define printd(x...) printf(x)
+#else
+#define printd(x...)
+#endif
+
+#define DEBUG_BACKUP_CACHE_SIZE 20
+
 struct cache_info {
 	BD_t * bd;
 	uint32_t size;
 	bdesc_t ** blocks;
+	uint32_t *dirty_bits;
 	uint16_t blocksize;
+	char *debug_cache[DEBUG_BACKUP_CACHE_SIZE];
+	int debug_dirty[DEBUG_BACKUP_CACHE_SIZE];
+	int debug_blkno[DEBUG_BACKUP_CACHE_SIZE];
 };
+
+unsigned short inet_chksum(void *, unsigned short);
+
+static void
+mark_dirty(BD_t * object, uint16_t bno)
+{
+	struct cache_info * info = (struct cache_info *) object->instance;
+	int course = bno/(8*sizeof(uint32_t));
+	int fine = bno % (8*sizeof(uint32_t));
+	info->dirty_bits[course] |= 1<<fine;
+}
+
+static bool
+is_dirty(BD_t * object, uint16_t bno)
+{
+	struct cache_info * info = (struct cache_info *) object->instance;
+	int course = bno/(8*sizeof(uint32_t));
+	int fine = bno % (8*sizeof(uint32_t));
+	return (info->dirty_bits[course] & (1<<fine)) != 0;
+}
+
+static void
+mark_clean(BD_t * object, uint16_t bno)
+{
+	struct cache_info * info = (struct cache_info *) object->instance;
+	int course = bno/(8*sizeof(uint32_t));
+	int fine = bno % (8*sizeof(uint32_t));
+	info->dirty_bits[course] &= ~(1<<fine);
+}
+
+static unsigned short
+blk_checksum(bdesc_t *blk)
+{
+	void *dataptr = blk->ddesc->data;
+	unsigned short len = blk->length;
+	return inet_chksum(dataptr, len);
+}
 
 static uint32_t
 wb_cache_bd_get_numblocks(BD_t * object)
@@ -68,7 +120,7 @@ reset_chdescs(chdesc_t *ch) {
  * precondition: CHDESC_MARKED is set to 1 for each chdesc in graph.
  * postcondition: CHDESC_MARKED is set to 0 for each chdesc in graph.
  */
-static void
+void
 reset_marks(chdesc_t *ch)
 {
 	chmetadesc_t *p;
@@ -77,6 +129,23 @@ reset_marks(chdesc_t *ch)
 	p = ch->dependencies;
 	while (p) {
 		reset_marks(p->desc);
+		p = p->next;
+	}
+}
+
+/*
+ * precondition: CHDESC_PRMARKED is set to 1 for each chdesc in graph.
+ * postcondition: CHDESC_PRMARKED is set to 0 for each chdesc in graph.
+ */
+void
+reset_prmarks(chdesc_t *ch)
+{
+	chmetadesc_t *p;
+	if (!(ch->flags & CHDESC_PRMARKED)) return;
+	ch->flags &= ~CHDESC_PRMARKED;
+	p = ch->dependencies;
+	while (p) {
+		reset_prmarks(p->desc);
 		p = p->next;
 	}
 }
@@ -104,13 +173,13 @@ number_chdescs(chdesc_t *ch, int num)
 }
 
 /*
- * precondition: CHDESC_MARKED is set to 0 for each chdesc in graph.
+ * precondition: CHDESC_PRMARKED is set to 0 for each chdesc in graph.
  *
- * postconditions: CHDESC_MAKRED is set to 1 for each chdesc in graph.
+ * postconditions: CHDESC_PRMAKRED is set to 1 for each chdesc in graph.
  *
  * printf() out all the dependencies.
  */
-static void
+void
 print_chdescs(chdesc_t *ch, int num)
 {
 	chmetadesc_t *p;
@@ -120,22 +189,33 @@ print_chdescs(chdesc_t *ch, int num)
 		printf("  ");
 	switch (ch->type) {
 	case BIT:
-		printf("ch: 0x%08x BIT dist %d block %d off 0x%x\n",
+		//printf("bit.\n");
+		//printf("0x%08x\n", ch);
+		//printf("0x%08x\n", ch->distance);
+		//printf("0x%08x\n", ch->block->number);
+		//printf("0x%08x\n", ch->byte.length);
+		printf("ch: 0x%08x BIT dist %d block %d off 0x%x",
 			   ch, ch->distance,
 			   ch->block->number, ch->bit.offset);
 		break;
 	case BYTE:
-		printf("ch: 0x%08x BYTE dist %d block %d off 0x%x len %d\n",
+		printf("ch: 0x%08x BYTE dist %d block %d off 0x%x len %d",
 			   ch, ch->distance, ch->block->number,
 			   ch->byte.offset, ch->byte.length);
 		break;
 	case NOOP:
-		printf("ch: 0x%08x NOOP dist %d\n", ch, ch->distance);
+		printf("ch: 0x%08x NOOP dist %d", ch, ch->distance);
 		break;
+	default:
+		printf("ch: 0x%08x UNKNOWN TYPE!! type: 0x%x", ch, ch->type);
 	}
 
-	if (ch->flags & CHDESC_MARKED) return;
-	ch->flags |= CHDESC_MARKED;
+	if (ch->flags & CHDESC_PRMARKED) {
+		printf(" (repeat)\n");
+		return;
+	} else
+		printf("\n");
+	ch->flags |= CHDESC_PRMARKED;
 
 	p = ch->dependencies;
 	while (p) {
@@ -144,11 +224,54 @@ print_chdescs(chdesc_t *ch, int num)
 	}
 }
 
+void
+print_chdescs_gv(chdesc_t *ch, int num)
+{
+	chmetadesc_t *p;
+	int i;
+
+	if (num == 0) {
+		printf("digraph chdescs\n{\nnodesep=0.15;\nranksep=0.15;\n"
+			"node [shape=circle,color=black];\n");
+	}
+
+	if (ch->flags & CHDESC_PRMARKED) {
+		return;
+	}
+	ch->flags |= CHDESC_PRMARKED;
+
+	switch (ch->type) {
+	case BIT:
+		printf("subgraph cluster%x {\n  n%x [label=\"%d;BIT\",fillcolor=slateblue1,style=filled]\n}\n",
+			   ch->block, ch, ch->distance);
+		break;
+	case BYTE:
+		printf("subgraph cluster%x {\n  n%x [label=\"%d;BYTE\",fillcolor=slateblue1,style=filled]\n}\n",
+			   ch->block, ch, ch->distance);
+		break;
+	case NOOP:
+		printf("n%x [label=\"%d;NOOP\",fillcolor=slateblue1,style=filled]\n",
+			   ch, ch->distance);
+		break;
+	}
+
+	p = ch->dependencies;
+	while (p) {
+		print_chdescs_gv(p->desc, num+1);
+		printf("\tn%x -> n%x [label=\"\"];\n", ch, p->desc);
+		p = p->next;
+	}
+	if (num == 0) {
+		printf("}\n");
+	}
+}
+
 /*
  * precondition: CHDESC_MARKED is set to 0 for each chdesc in graph.
  *
- * postconditions: CHDESC_MAKRED is set to 1 for each chdesc in graph,
- * heap contains all chdescs in graph.
+ * postconditions: CHDESC_MAKRED is set to 1 for all nodes from root
+ * to leaves, stopping at rolled-back chdecs. also, heap contains all
+ * such chdescs in graph.
  * 
  * the weight assigned to each chdesc in the graph is its distance.
  */
@@ -158,6 +281,7 @@ heapify_nodes(chdesc_t *ch, fixed_max_heap_t *heap)
 	chmetadesc_t *p;
 	if (ch->flags & CHDESC_MARKED) return;
 	ch->flags |= CHDESC_MARKED;
+	if (ch->flags & CHDESC_ROLLBACK) return;
 	fixed_max_heap_insert(heap, ch, ch->distance);
 	p = ch->dependencies;
 	while (p) {
@@ -215,7 +339,8 @@ int wb_cache_compare_chdescs(const void *a, const void *b)
 }
 
 /*
- * rollback all chdecs in block that meet any of these criteria:
+ * rollback all non-rolled-back chdecs in block that meet any of these
+ * criteria:
  *
  * - the chdesc is not in the heap
  * - the chdesc has out of block dependencies
@@ -223,7 +348,8 @@ int wb_cache_compare_chdescs(const void *a, const void *b)
  *
  * rolled back block will be inserted into 'rollback' so that they may
  * be later rolled forward. the chdescs that should be satisfied will
- * be inserted into 'satisfy'.
+ * be inserted into 'satisfy'. Blocks that were already rolled back
+ * are inserted into neither 'rollback' nor 'satisfy'.
  */
 static void
 rollback_block(bdesc_t *block, fixed_max_heap_t *heap,
@@ -244,6 +370,9 @@ rollback_block(bdesc_t *block, fixed_max_heap_t *heap,
 	// foreach chedsc ch in block:
 	for (p = root->dependencies; p != NULL; p = p->next) {
 		ch = p->desc;
+		if (ch->flags & CHDESC_ROLLBACK) continue; // skip already
+												   // rolled back
+												   // chdescs
 		vector_push_back(vect, ch);
 	}
 
@@ -287,6 +416,7 @@ rollback_block(bdesc_t *block, fixed_max_heap_t *heap,
 	for (i = 0; i < vector_size(vect); i++) {
 		ch = vector_elt(vect, i);
 		if (ch->flags & CHDESC_MARKED) {
+			ch->flags &= ~CHDESC_MARKED;
 			chdesc_rollback(ch);
 			r = vector_push_back(rollback, ch);
 			if (r < 0)
@@ -327,6 +457,7 @@ satisfy_chdescs(vector_t *vect, fixed_max_heap_t *heap)
 		chdesc_t *ch;
 		ch = vector_elt_end(vect);
 		if (ch) {
+			//printf("satisfying chdesc 0x%08x\n", ch);
 			depman_remove_chdesc(ch);
 			fixed_max_heap_delete(heap, ch);
 		}
@@ -347,6 +478,23 @@ wb_cache_bd_evict_block(BD_t *object, bdesc_t *block)
 	int count;
 	int value;
 
+	if (!is_dirty(object, block->number)) {
+		goto end;
+	}
+
+	root = (chdesc_t*)depman_get_deps(block); // shedding const
+											  // intentionally b/c of
+											  // CHDESC_MARKED flag.
+	if (root == NULL) {
+		// do the write
+		block->translated++;
+		block->bd = info->bd;
+		value = CALL(block->bd, write_block, block);
+		block->bd = object;
+		block->translated--;
+		goto end;
+	}
+
 	rollback = vector_create();
 	if (rollback == NULL) {
 		printf("%s: out of memory on vector_create()\n",
@@ -360,12 +508,6 @@ wb_cache_bd_evict_block(BD_t *object, bdesc_t *block)
 		return -E_NO_MEM;
 	}
 
-	root = (chdesc_t*)depman_get_deps(block); // shedding const
-											  // intentionally b/c of
-											  // CHDESC_MARKED flag.
-	if (root == NULL) {
-		goto end;
-	}
 	count = reset_chdescs(root);
 	reset_marks(root);
 	heap = fixed_max_heap_create(count);
@@ -375,16 +517,23 @@ wb_cache_bd_evict_block(BD_t *object, bdesc_t *block)
 	}
 
 	number_chdescs(root, 0); // calc max distance from nodes to root
-	//print_chdescs(root, 0);
-	//reset_marks(root);
+#if WB_CACHE_DEBUG_TREE
+	printf("tree for block %d:\n", block->number);
+	print_chdescs_gv(root, 0);
+	reset_prmarks(root);
+#endif
 	heapify_nodes(root, heap);
 	reset_marks(root);
+	//printf("heap contains %d chdescs\n", fixed_max_heap_length(heap));
 
 	// go through all nodes and commit their blocks
 	while (fixed_max_heap_length(heap)) {
 		chdesc_t *leaf;
 		bdesc_t *leafblock;
 		leaf = fixed_max_heap_head(heap);
+		if (leaf == NULL) {
+			printf("\n\n\nLEAF IS NULL\n\n\n");
+		}
 		if (leaf == root) {
 			break;
 		}
@@ -393,22 +542,48 @@ wb_cache_bd_evict_block(BD_t *object, bdesc_t *block)
 			depman_remove_chdesc(leaf);
 			continue;
 		}
+		//printf("popped leaf chdesc: 0x%08x\n", leaf);
 		leafblock = leaf->block;
+		if (leafblock == NULL)
+			printf("leafblock is NULL!\n");
+		//printf("rolling back\n");
 		rollback_block(leafblock, heap, rollback, satisfy);
+		//printf("done rolling back\n");
+#if WB_CACHE_DEBUG
+		if (vector_size(rollback)) {
+			printf("writing block w/ %d rolledback chdecs\n",
+				   vector_size(rollback));
+			printf("block refs: %d\n", leafblock->refs);
+		}
+		printf("writing block 0x%08x\n", leafblock);
+#endif
 
 		// do the write
-		leafblock->translated++;
-		leafblock->bd = info->bd;
-		value = CALL(leafblock->bd, write_block, leafblock);
-		leafblock->bd = object;
-		leafblock->translated--;
-		
+		//printf("satisfying chdescs\n");
 		satisfy_chdescs(satisfy, heap);
-		if (vector_size(rollback) == 0) {
-			// we're done w/ the leafblock!
-		} else
-			rollforward_chdescs(rollback);
+		//printf("done satisfying chdescs\n");
 		assert(vector_size(satisfy) == 0);
+
+		if (leafblock->bd == object) {
+			// a block in this cache. we write to the next lower bd
+			leafblock->translated++;
+			leafblock->bd = info->bd;
+			value = CALL(leafblock->bd, write_block, leafblock);
+			leafblock->bd = object;
+			leafblock->translated--;
+			if (vector_size(rollback) == 0) // we're done w/ the leafblock!
+				mark_clean(object, leafblock->number);
+		} else {
+			// block in another cache. just tell that cache to write
+			// it.
+			value = CALL(leafblock->bd, write_block, leafblock);
+		}
+		if (vector_size(rollback)) {
+			printf("post-write block refs: %d\n", leafblock->refs);
+		}
+		
+		if (vector_size(rollback) > 0)
+			rollforward_chdescs(rollback);
 		assert(vector_size(rollback) == 0);
 	}
 	// all done comitting!
@@ -416,7 +591,73 @@ wb_cache_bd_evict_block(BD_t *object, bdesc_t *block)
 	vector_destroy(rollback);
 	vector_destroy(satisfy);
  end:
+	mark_clean(object, block->number);
+	root = (chdesc_t*)depman_get_deps(block); // shedding const
+											  // intentionally b/c of
+											  // CHDESC_MARKED flag.
+	if (root != NULL) {
+		printf("problem tree?\n");
+		print_chdescs_gv(root, 0);
+		reset_prmarks(root);
+	}
 	bdesc_release(&block);
+	return 0;
+}
+
+static void
+dump_data(char *data, int len)
+{
+	int i, j;
+	return;
+	printf("data dump:\n");
+	for (i = 0; i < len; i+=32) {
+		printf("\t");
+		for (j = 0; j < 32; j+=4)
+			printf(" %08x", data[i + j]);
+		printf("\n");
+	}
+	printf("data dump done\n");
+}
+
+static void
+backup_insert(BD_t *object, bdesc_t *blk)
+{
+	struct cache_info * info = (struct cache_info *) object->instance;
+	int i;
+	for (i = 0; i < DEBUG_BACKUP_CACHE_SIZE; i++) {
+		if (info->debug_blkno[i] == blk->number ||
+			info->debug_blkno[i] == -1) {
+			info->debug_blkno[i] = blk->number;
+			memcpy(info->debug_cache[i], blk->ddesc->data, blk->length);
+			return;
+		}
+	}
+	//printf("BACKUP CACHE IS FULL!\n\n\n\n");
+}
+
+// return non zero if problem. 0 if no problem
+static bool
+backup_check(BD_t *object, bdesc_t *blk)
+{
+	struct cache_info * info = (struct cache_info *) object->instance;
+	int i;
+	int ret;
+	for (i = 0; i < DEBUG_BACKUP_CACHE_SIZE; i++) {
+		if (info->debug_blkno[i] == blk->number) {
+			ret = memcmp(info->debug_cache[i], blk->ddesc->data, blk->length);
+			if (ret != 0) {
+				printf("(actual checksum 0x%04x, should be 0x%04x)\n",
+					   blk_checksum(blk),
+					   inet_chksum(info->debug_cache[i],
+								   blk->length));
+				printf("you're asking:\n");
+				dump_data(blk->ddesc->data, blk->length);
+				printf("you should have:\n");
+				dump_data(info->debug_cache[i], blk->length);
+			}
+			return ret;
+		}
+	}
 	return 0;
 }
 
@@ -440,13 +681,17 @@ wb_cache_bd_read_block(BD_t * object, uint32_t number)
 		
 		// evict this cache entry
 		wb_cache_bd_evict_block(object, info->blocks[index]);
+	} else {
+
 	}
-	
 	/* not in the cache, need to read it */
 	info->blocks[index] = CALL(info->bd, read_block, number);
 	
-	if(!info->blocks[index])
+	if(!info->blocks[index]) {
+		printf("failed to read block number %d from lower level!\n",
+			   number);
 		return NULL;
+	}
 	
 	/* FIXME bdesc_alter() and bdesc_retain() can fail */
 	
@@ -455,6 +700,12 @@ wb_cache_bd_read_block(BD_t * object, uint32_t number)
 	
 	/* adjust the block descriptor to match the cache */
 	info->blocks[index]->bd = object;
+
+	/*
+	if (backup_check(object, info->blocks[index]) != 0) {
+		panic("read block %d failed!!\n", info->blocks[index]->number);
+	}
+	*/
 	
 	/* increase reference count */
 	bdesc_retain(&info->blocks[index]);
@@ -462,7 +713,6 @@ wb_cache_bd_read_block(BD_t * object, uint32_t number)
 	return info->blocks[index];
 }
 
-// XXX unfinished (i think) -adlr
 static int wb_cache_bd_write_block(BD_t * object, bdesc_t * block)
 {
 	struct cache_info * info = (struct cache_info *) object->instance;
@@ -481,19 +731,25 @@ static int wb_cache_bd_write_block(BD_t * object, bdesc_t * block)
 	if(block->number >= CALL(info->bd, get_numblocks))
 		return -E_INVAL;
 	
+	//backup_insert(object, block);
+	//printf("got a write for block %d\n", block->number);
+	
 	index = block->number % info->size;
 	if (info->blocks[index]->number == block->number) {
 		// overwrite existing block
-		value = bdesc_overwrite(block, info->blocks[index]);
+		value = bdesc_overwrite(info->blocks[index], block);
 		if (value < 0)
 			panic("bdesc_overwrite: %e\n", value);
+		bdesc_drop(&block);
 	} else {
 		// evict old block and write a new one
+		printd("cache conflict or miss. maybe evicting.\n");
 		if (info->blocks[index])
 			value = wb_cache_bd_evict_block(object, info->blocks[index]);
 		bdesc_retain(&block);
 		info->blocks[index] = block;
 	}
+	mark_dirty(object, info->blocks[index]->number);
 
 	return value;
 }
@@ -568,6 +824,8 @@ BD_t * wb_cache_bd(BD_t * disk, uint32_t blocks)
 {
 	struct cache_info * info;
 	BD_t * bd = malloc(sizeof(*bd));
+	int i;
+	
 	if(!bd)
 		return NULL;
 	
@@ -586,6 +844,28 @@ BD_t * wb_cache_bd(BD_t * disk, uint32_t blocks)
 		free(bd);
 		return NULL;
 	}
+	info->dirty_bits = malloc(blocks / (8*sizeof(uint32_t)) + 1);
+	if (info->dirty_bits == NULL) {
+		free(info->blocks);
+		free(info);
+		free(bd);
+		return NULL;
+	}
+	for (i = 0; i < blocks / (8*sizeof(uint32_t)) + 1; i++)
+		info->dirty_bits[i] = 0;
+
+	info->blocksize = CALL(disk, get_blocksize);
+	if (info->debug_cache == 0) {
+		return NULL;
+	}
+	for (i = 0; i < DEBUG_BACKUP_CACHE_SIZE; i++) {
+		info->debug_cache[i] = malloc(info->blocksize);
+		if (info->debug_cache[i] == 0)
+			return NULL;
+		info->debug_dirty[i] = 0;
+		info->debug_blkno[i] = -1;
+	}
+
 	memset(info->blocks, 0, blocks * sizeof(*info->blocks));
 	
 	ASSIGN(bd, wb_cache_bd, get_numblocks);
@@ -598,7 +878,6 @@ BD_t * wb_cache_bd(BD_t * disk, uint32_t blocks)
 	
 	info->bd = disk;
 	info->size = blocks;
-	info->blocksize = CALL(disk, get_blocksize);
 	
 	if(modman_add_anon_bd(bd, __FUNCTION__))
 	{
