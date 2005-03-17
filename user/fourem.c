11ed060e8ecc7005cd495100d2b2c68478f5d250
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2005-03-17 11:33:53 +0000

    r865: Updating fourem to use 4096K blocks instead of 512 bytes. Much more efficient, especially with write through caches.

--- user/fourem.c
+++ user/fourem.c
@@ -1,23 +1,24 @@
 #include <inc/lib.h>
 
-void
-umain(int argc, char **argv)
+static char data[4096];
+
+void umain(int argc, char **argv)
 {
-	int i, j, s;
-	char data[512];
-	int size;
-	for(i = 1; i < argc; i++) {
-		int fid = open(argv[i], O_CREAT|O_WRONLY);
+	int i;
+	for(i = 1; i < argc; i++)
+	{
+		int size, fid = open(argv[i], O_CREAT | O_WRONLY);
 		if(fid < 0)
 		{
 			printf("open %s: %e\n", argv[i], fid);
 			return;
 		}
 
-		size = 0;
-		for(j = 0; j < 8192; j++) {
-			s = write(fid, &data, 512);
-			if (s < 0) {
+		for(size = 0; size < 4 * 1024 * 1024; )
+		{
+			int s = write(fid, &data, sizeof(data));
+			if(s <= 0)
+			{
 				printf("write %s: %e\n", argv[i], s);
 				return;
 			}
