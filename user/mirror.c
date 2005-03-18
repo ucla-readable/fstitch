d00f3ae6c5b9ecb26d9b7b1bd968977778d6048a
Lei Zhang <leiz@featherstitch.ucla.edu>
2005-03-18 11:58:23 +0000

    r895: We can now use existing devices as arguments

--- user/mirror.c
+++ user/mirror.c
@@ -24,13 +24,15 @@ void print_usage(const char * bin)
 	printf("Usage:\n");
 	printf("%s create disk <controller> <diskno> <stride>\n", bin);
 	printf("%s create net <ip> <port> <stride>\n", bin);
+	printf("%s create bd <bd_name> <stride>\n", bin);
 	printf("%s add <mirror_bd> disk <controller> <diskno>\n", bin);
 	printf("%s add <mirror_bd> net <ip> <port>\n", bin);
+	printf("%s add <mirror_bd> bd <bd_name>\n", bin);
 	printf("%s remove mirror_bd <diskno>\n", bin);
 	exit();
 }
 
-BD_t * find_mirror(const char * mirror_name)
+BD_t * find_bd(const char * mirror_name)
 {
 	modman_it_t * it;
 	BD_t * c;
@@ -60,12 +62,13 @@ void umain(int argc, const char ** argv)
 		print_usage(argv[0]);
 
 	if (argc == 4 && strcmp(argv[1], "remove") == 0) {
+		mirror = find_bd(argv[2]);
 		int diskno = strtol(argv[3], NULL, 10);
 		if (mirror)
 			mirror_bd_remove_device(mirror, diskno);
 	}
 	else if (argc == 6 && strcmp(argv[1], "add") == 0) {
-		mirror = find_mirror(argv[2]);
+		mirror = find_bd(argv[2]);
 		if (!mirror)
 			exit();
 
@@ -84,6 +87,18 @@ void umain(int argc, const char ** argv)
 
 		mirror_bd_add_device(mirror, disk);
 	}
+	else if (argc == 5 && strcmp(argv[1], "add") == 0) {
+		mirror = find_bd(argv[2]);
+		if (!mirror)
+			exit();
+		if (!strcmp(argv[3], "bd"))
+			disk = find_bd(argv[4]);
+
+		if (!disk)
+			exit();
+
+		mirror_bd_add_device(mirror, disk);
+	}
 	else if (argc == 6 && strcmp(argv[1], "create") == 0) {
 		if (!strcmp(argv[2], "net")) {
 			int port = strtol(argv[4], NULL, 10);
@@ -96,8 +111,22 @@ void umain(int argc, const char ** argv)
 		}
 
 		int stride = strtol(argv[5], NULL, 10);
-		if (disk)
+		if (disk) {
+			printf("Creating mirror device\n");
 			mirror = mirror_bd(disk, disk, stride); // disk0 == disk1 -> disk1 = null
+		}
+
+		if (mirror)
+			printf("Mirror created\n");
+	}
+	else if (argc == 5 && strcmp(argv[1], "create") == 0) {
+		if (!strcmp(argv[2], "bd"))
+			disk = find_bd(argv[3]);
+		int stride = strtol(argv[4], NULL, 10);
+		if (disk) {
+			printf("Creating mirror device\n");
+			mirror = mirror_bd(disk, disk, stride); // disk0 == disk1 -> disk1 = null
+		}
 
 		if (mirror)
 			printf("Mirror created\n");
