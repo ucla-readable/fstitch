b44931fd0841d214d804311f163f22c2a01081aa
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2005-05-05 01:23:09 +0000

    r1028: Adding the autorelease prototypes and data structure changes to bdescs.

--- kfs/bdesc.h
+++ kfs/bdesc.h
@@ -20,35 +20,33 @@ struct datadesc {
 };
 
 struct bdesc {
-	uint32_t number, refs;
+	uint32_t number;
+	uint32_t ref_count;
+	uint32_t ar_count;
+	bdesc_t * ar_next;
 	datadesc_t * ddesc;
 };
 
 /* allocate a new bdesc */
 bdesc_t * bdesc_alloc(BD_t * bd, uint32_t number, uint16_t length);
 
-/* prepare a bdesc's data to be modified, copying its data if it is currently shared with another bdesc */
-int bdesc_touch(bdesc_t * bdesc);
-
-/* prepare the bdesc to be permanently translated ("altered") by copying it if it has nonzero reference count */
-int bdesc_alter(bdesc_t ** bdesc);
-
-/* increase the reference count of a bdesc, copying it if it is currently translated (but sharing the data) */
-int bdesc_retain(bdesc_t ** bdesc);
-
-/* free a bdesc if it has zero reference count */
-void bdesc_drop(bdesc_t ** bdesc);
-
-/* decrease the bdesc reference count but do not free it even if it reaches 0 */
-void bdesc_forget(bdesc_t ** bdesc);
+/* increase the reference count of a bdesc */
+bdesc_t * bdesc_retain(bdesc_t * bdesc);
 
 /* decrease the bdesc reference count and free it if it reaches 0 */
 void bdesc_release(bdesc_t ** bdesc);
 
+/* schedule the bdesc to be released at the end of the current run loop */
+bdesc_t * bdesc_autorelease(bdesc_t * bdesc);
+
+/* run the scheduled bdesc autoreleases */
+void bdesc_run_autorelease(void);
+
 /* a function for caches and cache-like modules to use for bdesc overwriting */
-int bdesc_overwrite(bdesc_t * cached, bdesc_t * written);
+/* this may no longer be needed, but it is left commented just in case */
+//int bdesc_overwrite(bdesc_t * cached, bdesc_t * written);
 
 /* compares two bdescs' blocknos for qsort */
-int bdesc_blockno_compare(const void * b1, const void * b2);
+int bdesc_blockno_compare(const void * a, const void * b);
 
 #endif /* __KUDOS_KFS_BDESC_H */
