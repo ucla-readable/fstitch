9def12bf38f197827f0bb82080091c71bef63c78
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2005-08-23 22:18:24 +0000

    r1299: More journal_bd code. Now it can recover a journal, though it can't create one.

--- kfs/journal_bd.c
+++ kfs/journal_bd.c
@@ -16,10 +16,15 @@ struct journal_info {
 	BD_t * bd;
 	BD_t * journal;
 	uint16_t blocksize, length;
-	uint16_t level;
+	uint16_t level, cr_count;
 	bool recursion;
 	chdesc_t * hold;
+	chdesc_t ** cr_commit;
+	uint32_t stamp;
 };
+
+#define TRANSACTION_PERIOD (5 * HZ)
+#define TRANSACTION_SIZE (64 * 4096)
 
 #define CREMPTY     0
 #define CRSUBCOMMIT 1
@@ -30,6 +35,20 @@ struct commit_record {
 	uint16_t type, next;
 	uint32_t nblocks;
 };
+
+/* number of block numbers that can be stored in a block */
+static uint16_t numbers_per_block(uint16_t blocksize)
+{
+	return blocksize / sizeof(uint32_t);
+}
+
+/* number of blocks that must be used for block numbers in a transaction */
+static uint32_t trans_number_block_count(uint16_t blocksize)
+{
+	const uint16_t npb = numbers_per_block(blocksize);
+	const uint32_t bpt = TRANSACTION_SIZE / blocksize;
+	return (bpt - 1 + npb) / (npb + 1);
+}
 
 static int journal_bd_get_config(void * object, int level, char * string, size_t length)
 {
@@ -138,6 +157,7 @@ static int journal_bd_write_block(BD_t * object, bdesc_t * block)
 				int r = chdesc_add_depend(meta->desc, info->hold);
 				if(r < 0)
 					panic("Holy Mackerel!");
+				chdesc_stamp(meta->desc, info->stamp);
 			}
 	}
 	
@@ -161,22 +181,179 @@ static void journal_bd_callback(void * arg)
 {
 	BD_t * object = (BD_t *) arg;
 	struct journal_info * info = (struct journal_info *) OBJLOCAL(object);
-	printf("Journal test: satisfying control NOOP!\n");
+	//printf("Journal test: satisfying control NOOP!\n");
 	if(info->hold)
 		chdesc_satisfy(&info->hold);
 }
 
 static int journal_bd_destroy(BD_t * bd)
 {
-	int r = modman_rem_bd(bd);
+	struct journal_info * info = (struct journal_info *) OBJLOCAL(bd);
+	int i, r = modman_rem_bd(bd);
 	if(r < 0)
 		return r;
-	modman_dec_bd(((struct journal_info *) OBJLOCAL(bd))->bd, bd);
-	modman_dec_bd(((struct journal_info *) OBJLOCAL(bd))->journal, bd);
+	modman_dec_bd(info->bd, bd);
+	modman_dec_bd(info->journal, bd);
 	sched_unregister(journal_bd_callback, bd);
-	free(OBJLOCAL(bd));
+	chdesc_release_stamp(info->stamp);
+	for(i = 0; i != info->cr_count; i++)
+		if(info->cr_commit[i])
+			chdesc_weak_release(&info->cr_commit[i]);
+	free(info->cr_commit);
+	free(info);
 	memset(bd, 0, sizeof(*bd));
 	free(bd);
+	return 0;
+}
+
+static int replay_single_transaction(BD_t * bd, uint32_t transaction_start, uint16_t expected_type)
+{
+	struct journal_info * info = (struct journal_info *) OBJLOCAL(bd);
+	chdesc_t * head = NULL;
+	chdesc_t * tail = NULL;
+	chdesc_t * keep = NULL;
+	chdesc_t * all = NULL;
+	int r;
+	
+	const uint32_t bnpb = numbers_per_block(info->blocksize);
+	const uint32_t transaction_blocks = TRANSACTION_SIZE / info->blocksize;
+	const uint32_t transaction_number = transaction_start / transaction_blocks;
+	
+	uint32_t block, bnb, db;
+	struct commit_record * cr;
+	bdesc_t * commit_block = CALL(info->journal, read_block, transaction_start);
+	
+	if(!commit_block)
+		return -E_UNSPECIFIED;
+	bdesc_retain(commit_block);
+	
+	cr = (struct commit_record *) commit_block->ddesc->data;
+	if(cr->magic != JOURNAL_MAGIC || cr->type != expected_type)
+	{
+		bdesc_release(&commit_block);
+		return 0;
+	}
+	
+	/* check for chained transaction */
+	block = cr->next * transaction_blocks;
+	if(block != transaction_start)
+	{
+		/* expect a CRSUBCOMMIT as the next element */
+		r = replay_single_transaction(bd, block, CRSUBCOMMIT);
+		if(r < 0)
+		{
+			bdesc_release(&commit_block);
+			return r;
+		}
+	}
+	
+	printf("%s(): recovering journal transaction %d (%u data blocks)\n", __FUNCTION__, transaction_number, cr->nblocks);
+	
+	keep = chdesc_create_noop(NULL, bd);
+	if(!keep)
+		panic("Holy Mackerel!");
+	chdesc_claim_noop(keep);
+	all = chdesc_create_noop(NULL, NULL);
+	if(!all)
+		panic("Holy Mackerel!");
+	r = chdesc_add_depend(all, keep);
+	if(r < 0)
+		panic("Holy Mackerel!");
+	
+	/* bnb is "block number block" number */
+	bnb = transaction_start + 1;
+	/* db is "data block" number */
+	db = bnb + trans_number_block_count(info->blocksize);
+	for(block = 0; block < cr->nblocks; block += bnpb)
+	{
+		uint32_t index, max = MIN(bnpb, cr->nblocks - block);
+		uint32_t * numbers;
+		bdesc_t * number_block = CALL(info->journal, read_block, bnb);
+		if(!number_block)
+		{
+			bdesc_release(&commit_block);
+			return -E_UNSPECIFIED;
+		}
+		bdesc_retain(number_block);
+		
+		numbers = (uint32_t *) number_block->ddesc->data;
+		for(index = 0; index != max; index++)
+		{
+			bdesc_t * output;
+			bdesc_t * data_block = CALL(info->journal, read_block, db++);
+			r = -E_UNSPECIFIED;
+			if(!data_block)
+				goto data_error;
+			bdesc_retain(data_block);
+			
+			/* FIXME synthetic */
+			output = CALL(info->bd, read_block, numbers[index]);
+			if(!output)
+				goto output_error;
+			
+			head = NULL;
+			r = chdesc_create_full(output, info->bd, data_block->ddesc->data, &head, &tail);
+			if(r < 0)
+				goto output_error;
+			r = chdesc_add_depend(all, head);
+			if(r < 0)
+				goto chdesc_error;
+			r = CALL(info->bd, write_block, output);
+			if(r < 0)
+				goto chdesc_error;
+			bdesc_release(&data_block);
+			continue;
+			
+		chdesc_error:
+			/* FIXME clean up chdescs */
+			assert(0);
+		output_error:
+			bdesc_release(&data_block);
+		data_error:
+			bdesc_release(&number_block);
+			bdesc_release(&commit_block);
+			return r;
+		}
+		
+		bdesc_release(&number_block);
+	}
+	
+	/* only CRCOMMIT records need to be cancelled */
+	if(cr->type == CRCOMMIT)
+	{
+		typeof(cr->type) empty = CREMPTY;
+		head = all;
+		chdesc_create_byte(commit_block, info->journal, (uint16_t) &((struct commit_record *) NULL)->type, sizeof(cr->type), &empty, &head, &tail);
+		assert(head == tail);
+		r = chdesc_weak_retain(head, &info->cr_commit[transaction_start / transaction_blocks]);
+		if(r < 0)
+			panic("Holy Mackerel!");
+		chdesc_satisfy(&keep);
+		r = CALL(info->journal, write_block, commit_block);
+		if(r < 0)
+			panic("Holy Mackerel!");
+	}
+	else
+		chdesc_satisfy(&keep);
+	
+	bdesc_release(&commit_block);
+	
+	return 0;
+}
+
+static int replay_journal(BD_t * bd)
+{
+	struct journal_info * info = (struct journal_info *) OBJLOCAL(bd);
+	uint32_t transaction, transaction_blocks = TRANSACTION_SIZE / info->blocksize;
+	
+	for(transaction = 0; transaction < info->cr_count; transaction++)
+	{
+		/* FIXME this may need attention when we finally fix the transaction ordering bug */
+		int r = replay_single_transaction(bd, transaction * transaction_blocks, CRCOMMIT);
+		if(r < 0)
+			return r;
+	}
+	
 	return 0;
 }
 
@@ -209,6 +386,14 @@ BD_t * journal_bd(BD_t * disk, BD_t * journal)
 		return NULL;
 	}
 	
+	info->stamp = chdesc_register_stamp(bd);
+	if(!info->stamp)
+	{
+		free(info);
+		free(bd);
+		return NULL;
+	}
+	
 	BD_INIT(bd, journal_bd, info);
 	OBJMAGIC(bd) = JOURNAL_MAGIC;
 	
@@ -221,8 +406,18 @@ BD_t * journal_bd(BD_t * disk, BD_t * journal)
 	
 	info->level = CALL(disk, get_devlevel);
 	
-	/* sixty second callback */
-	if(sched_register(journal_bd_callback, bd, 60 * HZ) < 0)
+	info->cr_count = CALL(journal, get_numblocks) / (TRANSACTION_SIZE / blocksize);
+	if(!info->cr_count)
+		panic("Holy Mackerel!");
+	
+	info->cr_commit = calloc(info->cr_count, sizeof(*info->cr_commit));
+	if(!info->cr_commit)
+		panic("Holy Mackerel!");
+	
+	replay_journal(bd);
+	
+	/* set up transaction callback */
+	if(sched_register(journal_bd_callback, bd, TRANSACTION_PERIOD) < 0)
 	{
 		DESTROY(bd);
 		return NULL;
