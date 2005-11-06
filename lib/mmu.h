629c5099dbe3f7ee73422111715a83410a952ddd
Chris Frost <frost@cs.ucla.edu>
2005-11-06 09:55:10 +0000

    r1596: mmu.h, now smarter: discover compile-time pagesize for linux and mach. We could also discover the runtime pagesize, but kudos isn't setup for this.

--- lib/mmu.h
+++ lib/mmu.h
@@ -3,9 +3,22 @@
 
 #if defined(KUDOS)
 #include <inc/mmu.h>
+
+#elif defined(UNIXUSER)
+
+#if defined(__linux__)
+#include <asm/page.h>
+#define PGSIZE PAGE_SIZE
+#elif defined(__MACH__)
+#include <sys/vm.h>
+#define PGSIZE PAGE_SIZE
 #else
 #warning Assuming 4KB page size
 #define PGSIZE 4096
+#endif
+
+#else
+#error Unknown target system
 #endif
 
 #endif /* !KUDOS_LIB_MMU_H */
