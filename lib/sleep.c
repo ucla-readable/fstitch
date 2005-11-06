7179b23b743b2d52fbbceb20d383f8a398613ffc
Andrew de los Reyes <andrew-featherstitchgit@gizmolabs.org>
2005-11-06 03:35:27 +0000

    r1588: sleepj for UU

--- lib/sleep.c
+++ lib/sleep.c
@@ -1,8 +1,15 @@
+#if defined(KUDOS)
 #include <inc/lib.h>
+#elif defined(UNIXUSER)
+#include <unistd.h>
+#endif
 
 int
 sleepj(int32_t jiffies)
 {
+#if defined(UNIXUSER)
+	return usleep(jiffies * 10000);
+#elif defined(KUDOS)
 	const int32_t wakeup = jiffies + env->env_jiffies;
 
 	if (jiffies < 0)
@@ -14,4 +21,5 @@ sleepj(int32_t jiffies)
 			return 0;
 		sys_yield();
 	}
+#endif
 }
