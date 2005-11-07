351272fc37eccf077f852fc3abf0bef086035496
Andrew de los Reyes <andrew-featherstitchgit@gizmolabs.org>
2005-11-07 07:30:37 +0000

    r1609: kdprintf is now implemented by us. it depends on vasprintf()

--- lib/kdprintf.c
+++ lib/kdprintf.c
@@ -1,6 +1,8 @@
 // XXX change this to use printfmt properly,
 // with no string length limitation.
 
+#if defined(KUDOS)
+
 #include <inc/lib.h>
 
 int
@@ -38,4 +40,36 @@ printf(const char *fmt, ...)
 		va_end(ap);
 		return cnt;
 	}
- }
+}
+
+#elif defined(UNIXUSER)
+
+#if defined(__linux__)
+#define _GNU_SOURCE
+#endif
+#include <stdio.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int
+kdprintf(int fd, const char *fmt, ...)
+{
+	char *buf;
+	int r;
+	va_list ap;
+
+	va_start(ap, fmt);
+	vasprintf(&buf, fmt, ap);
+	assert(buf);
+	va_end(ap);
+	r = write(fd, buf, strlen(buf));
+	free(buf);
+	return r;
+}
+
+#else
+#error Unknown target system
+#endif
--- lib/kdprintf.h
+++ lib/kdprintf.h
@@ -5,34 +5,11 @@
 #include <inc/stdio.h>
 
 #elif defined(UNIXUSER)
-#define _GNU_SOURCE
-#include <stdio.h> // [v]dprintf
+#include <stdio.h> // [v]printf
 #include <stdarg.h> // va_list et al
 #include <unistd.h> // STD[IN|OUT|ERR]_FILENO
-//#if defined(dprintf)
 
-// FIXME: we can't reinclude stdio.h if it was already included
-extern int dprintf(int, const char*, ...);
-extern int vdprintf(int, const char*, va_list);
-
-static __inline int kdprintf(int fd, const char*, ...) __attribute__((always_inline));
-static __inline int kdprintf(int fd, const char *format, ...)
-{
-	va_list ap;
-	va_start(ap, format);
-	int r = vdprintf(fd, format, ap);
-	va_end(ap);
-	return r;
-}
-static __inline int vkdprintf(int fd, const char*, va_list) __attribute__((always_inline));
-static __inline int vkdprintf(int fd, const char *format, va_list ap)
-{
-	return vdprintf(fd, format, ap);
-}
-//#else
-//#error Your os needs [v]kdprintf implementations
-//#endif
-
+int	kdprintf(int fd, const char*, ...);
 #else
 #error Unknown target system
 #endif
