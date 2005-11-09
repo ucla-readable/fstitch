567de430995d08247552b25ca78317e0d7d70128
Chris Frost <frost@cs.ucla.edu>
2005-11-09 19:03:59 +0000

    r1614: Added a few UU implementation idea notes.

--- kfs/ipc_serve.c
+++ kfs/ipc_serve.c
@@ -61,8 +61,24 @@ void ipc_serve_run(void)
 
 int ipc_serve_init(void)
 {
-	// TODO: implement this module. Perhaps use SysV IPC messages?
-	kdprintf(STDERR_FILENO, "ipc_serve not yet implmented for unix-user\n");
+	// TODO: implement this module.
+	//
+	// Perhaps use SysV IPC messages or a combination of SysV IPC shared
+	// memory and semaphores?
+	// - messages make (as of a few years ago) 2 data copies along the way.
+	//   kudos ipc makes none, but because of kudos ipc uses shared memory
+	//   cfs_ipc_client library makes 1 copy.
+	// - do they differ in the number of context switches?
+	// - we might consider how ipc will affect our move to async. eg being
+	//   able to select()/kqueue() incoming messages may be easier to work
+	//   with than just a syscall to receive data.
+	// - are there other performance or functionality points we should
+	//   consider?
+	// - sysv ipc messages and shared memory allow us to transfer more than
+	//   one page at a time. we can use this to reduce reads and writes
+	//   from 2 to 1 data-transfer ipc.
+
+	kdprintf(STDERR_FILENO, "ipc_serve not yet implemented for unix-user\n");
 	return 0;
 }
 
