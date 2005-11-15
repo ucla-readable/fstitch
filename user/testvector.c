dedf1beb2db8b06bd69c071521f5312cbed300d5
Chris Frost <frost@cs.ucla.edu>
2005-11-15 05:12:18 +0000

    r1623: Adding two (not part of the build) test programs for hash_map_t and vector_t. These are from the CVS days (2005 mid-February).

---
+++ user/testvector.c
@@ -0,0 +1,19 @@
+#include <inc/lib.h>
+#include <inc/vector.h>
+
+void umain(void)
+{
+	int r;
+	vector_t * v = vector_create();
+	if (!v)
+		printf("vector_create() = NULL\n");
+
+	int a = 5;
+
+	if ((r = vector_push_back(v, &a)) < 0)
+		printf("vector_push_back() FAILED: %e\n", r);
+
+	int * pa = vector_elt(v, 0);
+	if (pa != &a)
+		printf("vector_elt() FAILED (%x)\n", pa);
+}
