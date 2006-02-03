92d1104c7434270121897b31bf7e487f66b6fef4
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2006-02-03 10:06:35 +0000

    r1746: Oops. This was supposed to be part of r1745.

---
+++ kfs/sync.c
@@ -0,0 +1,26 @@
+#include <inc/error.h>
+#include <stdio.h>
+
+#include <kfs/bd.h>
+#include <kfs/modman.h>
+#include <kfs/sync.h>
+#include <kfs/modman.h>
+
+int kfs_sync(const char * name)
+{
+	for(;;)
+	{
+		BD_t * bd;
+		modman_it_t it;
+		int r = FLUSH_EMPTY;
+		
+		modman_it_init_bd(&it);
+		while((bd = modman_it_next_bd(&it)))
+			r |= CALL(bd, flush, FLUSH_DEVICE, NULL);
+		
+		if(r == FLUSH_EMPTY)
+			return 0;
+		if(r == FLUSH_NONE)
+			return -E_BUSY;
+	}
+}
---
+++ kfs/sync.h
@@ -0,0 +1,6 @@
+#ifndef __KUDOS_KFS_SYNC_H
+#define __KUDOS_KFS_SYNC_H
+
+int kfs_sync(const char * name);
+
+#endif /* __KUDOS_KFS_SYNC_H */
