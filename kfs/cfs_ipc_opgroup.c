15901f4ec7e3b34ea1475b6014c0fe5417cbe83f
Chris Frost <frost@cs.ucla.edu>
2006-02-07 04:58:48 +0000

    r1782: - Many simplifications to base dead/existence/etc (plus bug fixes to prior,   more complex code) - Bug fixes for child env scopes - Env scope garbage collection

--- kfs/cfs_ipc_opgroup.c
+++ kfs/cfs_ipc_opgroup.c
@@ -1,13 +1,15 @@
 #include <assert.h>
 #include <inc/lib.h>
+#include <lib/jiffies.h>
 #include <kfs/modman.h>
+#include <kfs/sched.h>
 #include <kfs/opgroup.h>
 #include <kfs/cfs_ipc_opgroup.h>
 
 /* just the cfs_ipc_opgroup_scope_* functions */
 #define CFS_IPC_OPGROUP_SCOPE_DEBUG 0
 /* just the cfs_ipc_opgroup_* functions (does not include scope functions) */
-#define CFS_IPC_OPGROUP_DEBUG 1
+#define CFS_IPC_OPGROUP_DEBUG 0
 
 #if CFS_IPC_OPGROUP_SCOPE_DEBUG
 #define DSprintf(x...) printf(x)
@@ -21,6 +23,8 @@
 #define Dprintf(x...)
 #endif
 
+#define OPGROUP_SCOPE_GC_PERIOD (10*HZ)
+
 
 //
 // cfs_ipc_opgroup_scopes
@@ -28,6 +32,7 @@
 
 typedef struct scope_entry {
 	opgroup_scope_t * scope;
+	envid_t client;
 	uintptr_t client_scope_va;
 } scope_entry_t;
 
@@ -39,60 +44,54 @@ static bool va_is_mapped(const void * va)
 	return (vpd[PDX(va)] & PTE_P) && (vpt[VPN(va)] & PTE_P);
 }
 
+static bool env_page_is_mapped(const void * pg, envid_t envid, uintptr_t envid_va)
+{
+	int r = sys_page_is_mapped((void *) pg, envid, envid_va);
+	if (r < 0 && r != -E_BAD_ENV)
+		panic("sys_page_is_mapped(0x%08x, %08x, 0x%08x): %e\n", pg, envid, envid_va);
+	return r == 1;
+}
+
 static const void * env_scope_cappg(envid_t envid)
 {
 	return CFS_IPC_OPGROUP_SCOPE_CAPPGS + ENVX(envid)*PGSIZE;
 }
 
-static bool env_scope_cappg_is_dead(envid_t envid)
+static void env_scope_clear(size_t index)
 {
-	int refcount;
+	assert(index < NENV);
+	env_scopes[index].scope = NULL;
+	env_scopes[index].client_scope_va = 0;
+	env_scopes[index].client = 0;
+}
+
+static void env_scope_set(envid_t client, opgroup_scope_t * scope, uintptr_t client_scope_va)
+{
+	assert(client && scope);
+	env_scopes[ENVX(client)].scope = scope;
+	env_scopes[ENVX(client)].client_scope_va = client_scope_va;
+	env_scopes[ENVX(client)].client = client;
+}
+
+// An env scope is dead if it was alive and has since died
+// (died = exited or otherwise unmapped the page)
+static bool env_scope_is_dead(envid_t envid)
+{
+	bool is_alive;
+	int nrefs;
 
 	if (!va_is_mapped(env_scope_cappg(envid)))
 		return 0;
 
-	switch ((refcount = pageref(env_scope_cappg(envid))))
-	{
-		case 2: return 0;
-		case 1: return 1;
-		case 0:
-			assert(0); // can't happen
-		default:
-			panic("%s: opgroup scope page not designed to have >2 (%d) refs\n", __FUNCTION__, refcount);
-	}
-}
-
-static void env_scope_init(envid_t envid, opgroup_scope_t * scope, uintptr_t client_scope_va)
-{
-	env_scopes[ENVX(envid)].scope = scope;
-	env_scopes[ENVX(envid)].client_scope_va = client_scope_va;
-}
-
-// An env scope is valid if:
-// (1) the env has an scope entry and the entry is alive and is mapped
-//     into the client at the correct address
-// (2) the env has no scope entry
-static bool env_scope_is_valid(envid_t envid)
-{
-	int r;
-
-	if (env_scope_cappg_is_dead(envid))
-		return 0;
-	if (!va_is_mapped(env_scope_cappg(envid)))
-		return 1;
-
-	r = sys_page_is_mapped((void *) env_scope_cappg(envid), envid, env_scopes[ENVX(envid)].client_scope_va);
-	if (r < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(%08x): sys_page_is_mapped(): %e\n", __FUNCTION__, envid, r);
-		sys_print_backtrace();
-	}
-	return r == 1;
+	is_alive = env_page_is_mapped(env_scope_cappg(envid), envid, env_scopes[ENVX(envid)].client_scope_va);
+	if (is_alive && ((nrefs = pageref(env_scope_cappg(envid))) != 2))
+		panic("opgroup scope page not designed to have >2 (%d) refs\n", nrefs);
+	return !is_alive;
 }
 
 static bool env_scope_exists(envid_t envid)
 {
-	return env_scope_is_valid(envid) && va_is_mapped(env_scope_cappg(envid));
+	return (!env_scope_is_dead(envid)) && va_is_mapped(env_scope_cappg(envid));
 }
 
 static opgroup_scope_t * env_scope(envid_t envid)
@@ -105,28 +104,30 @@ static opgroup_scope_t * env_scope(envid_t envid)
 static int env_scope_destroy(envid_t envid)
 {
 	int r;
+	DSprintf("%s(env_slot = 0x%03x)\n", __FUNCTION__, ENVX(envid));
 
-	if (pageref(env_scope_cappg(envid)) > 1)
-		kdprintf(STDERR_FILENO, "%s(%08x): env scope's cappg still has references, destroying anyway\n", __FUNCTION__, envid);
+	if (env_scope_exists(envid))
+		kdprintf(STDERR_FILENO, "%s(%08x): env scope is still valid, destroying anyway\n", __FUNCTION__, envid);
+	else if ((r = pageref(env_scope_cappg(envid))) > 1)
+		kdprintf(STDERR_FILENO, "%s(%08x): env scope's cappg still has %d references, destroying anyway\n", __FUNCTION__, envid, r);
 
 	if ((r = sys_page_unmap(0, (void *) env_scope_cappg(envid))) < 0)
 		return r;
-	if (env_scopes[ENVX(envid)].scope)
-		opgroup_scope_destroy(env_scopes[ENVX(envid)].scope);
-	env_scope_init(envid, NULL, 0);
+	opgroup_scope_destroy(env_scopes[ENVX(envid)].scope);
+	env_scope_clear(ENVX(envid));
 	return 0;
 }
 
 
-int set_cur_opgroup_scope(envid_t envid)
+static int set_cur_opgroup_scope(envid_t envid)
 {
-	if (!env_scope_is_valid(envid))
-		return -E_BAD_ENV; // error for now, but we might want to init
+	if (env_scope_is_dead(envid))
+		return -E_BAD_ENV; // error for now, but we might want to create
 	opgroup_scope_set_current(env_scope(envid));
 	return 0;
 }
 
-void clear_cur_opgroup_scope(void)
+static void clear_cur_opgroup_scope(void)
 {
 	opgroup_scope_set_current(NULL);
 }
@@ -141,13 +142,13 @@ int cfs_ipc_opgroup_scope_create(envid_t envid, const void * scope_cappg, uintpt
 	if (!envid_scope_cappg_va)
 		return -E_INVAL;
 
-	if (sys_page_is_mapped((void *) scope_cappg, envid, envid_scope_cappg_va) < 1)
+	if (!env_page_is_mapped(scope_cappg, envid, envid_scope_cappg_va))
 		return -E_INVAL;
 
 	if (env_scope_exists(envid))
 		return -E_UNSPECIFIED; // disallow re-creation
 
-	if (env_scope_cappg_is_dead(envid))
+	if (env_scope_is_dead(envid))
 	{
 		r = env_scope_destroy(envid);
 		assert(r >= 0);
@@ -162,7 +163,7 @@ int cfs_ipc_opgroup_scope_create(envid_t envid, const void * scope_cappg, uintpt
 		return -E_NO_MEM;
 	}
 
-	env_scope_init(envid, env_scope, envid_scope_cappg_va);
+	env_scope_set(envid, env_scope, envid_scope_cappg_va);
 
 	return 0;
 }
@@ -178,13 +179,14 @@ int cfs_ipc_opgroup_scope_copy(envid_t parent, envid_t child, const void * child
 		return -E_INVAL;
 	if (!env_scope_exists(parent))
 		return -E_BAD_ENV;
-	if (env_scope_exists(child))
+	if (env_scope_exists(child) && !env_scope_is_dead(child))
 		return -E_INVAL; // disallow re-creation
-	if (sys_page_is_mapped((void *) child_scope_cappg, child, child_scope_cappg_va) < 1)
+	if (!env_page_is_mapped(child_scope_cappg, child, child_scope_cappg_va))
 		return -E_INVAL;
 
-	if (env_scope_cappg_is_dead(child))
+	if (env_scope_is_dead(child))
 	{
+		DSprintf("%s: ", __FUNCTION__); // prepend env_scope_destroy's output
 		r = env_scope_destroy(child);
 		assert(r >= 0);
 	}
@@ -198,7 +200,7 @@ int cfs_ipc_opgroup_scope_copy(envid_t parent, envid_t child, const void * child
 		return -E_NO_MEM;
 	}
 
-	env_scope_init(child, child_env_scope, child_scope_cappg_va);
+	env_scope_set(child, child_env_scope, child_scope_cappg_va);
 
 	return 0;
 }
@@ -226,10 +228,20 @@ static opgroupscope_tracker_state_t this_state;
 static CFS_t this_cfs;
 
 
-static void open_opgroup_scope_gc(void)
+static void opgroup_scope_gc(void * ignore)
 {
+	int i, r;
 	DSprintf("%s()\n", __FUNCTION__);
-	panic("TODO");
+	for (i = 0; i < NENV; i++)
+	{
+		envid_t envid = env_scopes[i].client;
+		if (env_scope_is_dead(envid))
+		{
+			r = env_scope_destroy(envid);
+			assert(r >= 0);
+			env_scope_clear(ENVX(envid));
+		}
+	}
 }
 
 
@@ -253,7 +265,7 @@ static int opgroupscope_tracker_get_status(void * object, int level, char * stri
 	size_t i, nenvs;
 
 	for (i = 0, nenvs = 0; i < NENV; i++)
-		if (va_is_mapped(CFS_IPC_OPGROUP_SCOPE_CAPPGS + i*PGSIZE))
+		if (env_scope_exists(env_scopes[i].client))
 			nenvs++;
 	
 	snprintf(string, length, "envs: %u", nenvs);
@@ -291,7 +303,10 @@ static int opgroupscope_tracker_destroy(CFS_t * cfs)
 		return r;
 	modman_dec_cfs(this_state.frontend_cfs, cfs);
 
-	open_opgroup_scope_gc();
+	// ignore return because constructor can call before gc is registered
+	(void) sched_unregister(opgroup_scope_gc, NULL);
+
+	opgroup_scope_gc(NULL);
 	for (i = 0; i < NENV; i++)
 		if (va_is_mapped(CFS_IPC_OPGROUP_SCOPE_CAPPGS + i*PGSIZE))
 			kdprintf(STDERR_FILENO, "%s: cappg %u still mapped\n", __FUNCTION__, i);
@@ -441,6 +456,7 @@ static int opgroupscope_tracker_set_metadata(CFS_t * cfs, const char * name, uin
 CFS_t * opgroupscope_tracker_cfs(CFS_t * frontend_cfs)
 {
 	size_t i;
+	int r;
 
 	if (opgroupscope_tracker_cfs_exists)
 		panic("opgroupscope_tracker_cfs can currently have at most one instance.");
@@ -456,6 +472,12 @@ CFS_t * opgroupscope_tracker_cfs(CFS_t * frontend_cfs)
 			panic("%s: cappg %u is in use.\n", __FUNCTION__, i);
 	memset(env_scopes, 0, sizeof(env_scopes));
 	this_state.frontend_cfs = frontend_cfs;
+
+	if ((r = sched_register(opgroup_scope_gc, NULL, OPGROUP_SCOPE_GC_PERIOD)) < 0)
+	{
+		DESTROY(&this_cfs);
+		return NULL;
+	}
 
 	if(modman_add_anon_cfs(&this_cfs, __FUNCTION__))
 	{
@@ -480,17 +502,30 @@ CFS_t * opgroupscope_tracker_cfs(CFS_t * frontend_cfs)
 // cfs_ipc_opgroup
 
 
+static int set_cur_opgroup_scope_wrap(envid_t envid, const char * call_fn)
+{
+	int r;
+	if (!env_scope_exists(envid))
+	{
+		kdprintf(STDERR_FILENO, "%s(env = %08x): env has no scope\n", __FUNCTION__, envid);
+		return -E_BAD_ENV;
+	}
+
+	r = set_cur_opgroup_scope(envid);
+	assert(r >= 0);
+
+	return 0;
+}
+
+
 opgroup_id_t cfs_ipc_opgroup_create(envid_t envid, int flags)
 {
+	int r;
 	opgroup_t * opgroup;
 	opgroup_id_t opgroupid;
-	int r = -E_BAD_ENV;
-	if (!env_scope_exists(envid) || (r = set_cur_opgroup_scope(envid)) < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(env = %08x, flags = %d): env has no scope\n", __FUNCTION__, envid, flags);
-		return r;
-	}
 	Dprintf("%s(env = %08x, flags = %d)\n", __FUNCTION__, envid, flags);
+	if ((r = set_cur_opgroup_scope_wrap(envid, __FUNCTION__)))
+		return r;
 	opgroup = opgroup_create(flags);
 	opgroupid = opgroup_id(opgroup);
 	clear_cur_opgroup_scope();
@@ -500,13 +535,10 @@ opgroup_id_t cfs_ipc_opgroup_create(envid_t envid, int flags)
 
 int cfs_ipc_opgroup_add_depend(envid_t envid, opgroup_id_t dependent_id, opgroup_id_t dependency_id)
 {
-	int r = -E_BAD_ENV;
-	if (!env_scope_exists(envid) || (r = set_cur_opgroup_scope(envid)) < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(env = %08x, dependent_id = %d, dependency_id = %d): env has no scope\n", __FUNCTION__, envid, dependent_id, dependency_id);
-		return r;
-	}
+	int r;
 	Dprintf("%s(env = %08x, dependent_id = %d, dependency_id = %d)\n", __FUNCTION__, envid, dependent_id, dependency_id);
+	if ((r = set_cur_opgroup_scope_wrap(envid, __FUNCTION__)))
+		return r;
 	r = opgroup_add_depend(opgroup_lookup(dependent_id), opgroup_lookup(dependency_id));
 	clear_cur_opgroup_scope();
 	return r;
@@ -514,13 +546,10 @@ int cfs_ipc_opgroup_add_depend(envid_t envid, opgroup_id_t dependent_id, opgroup
 
 int cfs_ipc_opgroup_engage(envid_t envid, opgroup_id_t opgroupid)
 {
-	int r = -E_BAD_ENV;
-	if (!env_scope_exists(envid) || (r = set_cur_opgroup_scope(envid)) < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(env = %08x, opgroupid = %d): env has no scope\n", __FUNCTION__, envid, opgroupid);
-		return r;
-	}
+	int r;
 	Dprintf("%s(env = %08x, opgroupid = %d)\n", __FUNCTION__, envid, opgroupid);
+	if ((r = set_cur_opgroup_scope_wrap(envid, __FUNCTION__)))
+		return r;
 	r = opgroup_engage(opgroup_lookup(opgroupid));
 	clear_cur_opgroup_scope();
 	return r;
@@ -528,13 +557,10 @@ int cfs_ipc_opgroup_engage(envid_t envid, opgroup_id_t opgroupid)
 
 int cfs_ipc_opgroup_disengage(envid_t envid, opgroup_id_t opgroupid)
 {
-	int r = -E_BAD_ENV;
-	if (!env_scope_exists(envid) || (r = set_cur_opgroup_scope(envid)) < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(env = %08x, opgroupid = %d): env has no scope\n", __FUNCTION__, envid, opgroupid);
-		return r;
-	}
+	int r;
 	Dprintf("%s(env = %08x, opgroupid = %d)\n", __FUNCTION__, envid, opgroupid);
+	if ((r = set_cur_opgroup_scope_wrap(envid, __FUNCTION__)))
+		return r;
 	r = opgroup_disengage(opgroup_lookup(opgroupid));
 	clear_cur_opgroup_scope();
 	return r;
@@ -542,13 +568,10 @@ int cfs_ipc_opgroup_disengage(envid_t envid, opgroup_id_t opgroupid)
 
 int cfs_ipc_opgroup_release(envid_t envid, opgroup_id_t opgroupid)
 {
-	int r = -E_BAD_ENV;
-	if (!env_scope_exists(envid) || (r = set_cur_opgroup_scope(envid)) < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(env = %08x, opgroupid = %d): env has no scope\n", __FUNCTION__, envid, opgroupid);
-		return r;
-	}
+	int r;
 	Dprintf("%s(env = %08x, opgroupid = %d)\n", __FUNCTION__, envid, opgroupid);
+	if ((r = set_cur_opgroup_scope_wrap(envid, __FUNCTION__)))
+		return r;
 	r = opgroup_release(opgroup_lookup(opgroupid));
 	clear_cur_opgroup_scope();
 	return r;
@@ -556,14 +579,11 @@ int cfs_ipc_opgroup_release(envid_t envid, opgroup_id_t opgroupid)
 
 int cfs_ipc_opgroup_abandon(envid_t envid, opgroup_id_t opgroupid)
 {
-	int r = -E_BAD_ENV;
+	int r;
 	opgroup_t * opgroup;
-	if (!env_scope_exists(envid) || (r = set_cur_opgroup_scope(envid)) < 0)
-	{
-		kdprintf(STDERR_FILENO, "%s(env = %08x, opgroupid = %d): env has no scope\n", __FUNCTION__, envid, opgroupid);
-		return r;
-	}
 	Dprintf("%s(env = %08x, opgroupid = %d)\n", __FUNCTION__, envid, opgroupid);
+	if ((r = set_cur_opgroup_scope_wrap(envid, __FUNCTION__)))
+		return r;
 	opgroup = opgroup_lookup(opgroupid);
 	r = opgroup_abandon(&opgroup);
 	clear_cur_opgroup_scope();
