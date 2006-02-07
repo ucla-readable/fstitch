7433eee629ecf7090430950d316e9861e7856e3e
Chris Frost <frost@cs.ucla.edu>
2006-02-07 05:01:27 +0000

    r1784: When printing spawn failure, display our envid to possibly help make future bug tracking faster.

--- user/init.c
+++ user/init.c
@@ -9,5 +9,5 @@ umain(void)
 	sys_env_set_name(0, "init");
 
 	if ((r = spawnl("/sh", "/sh", "/init.sh", (const char **) 0)) < 0)
-		kdprintf(STDERR_FILENO, "spawn /init.sh: %e\n", r);
+		kdprintf(STDERR_FILENO, "[%08x] spawn /init.sh: %e\n", env->env_id, r);
 }
