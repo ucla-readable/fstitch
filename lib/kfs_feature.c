15a539c58d4c17c52617aceda386a98acd1fc4a7
Chris Frost <frost@cs.ucla.edu>
2006-02-22 07:07:22 +0000

    r1906: This should have been part of r1905.

--- lib/kfs_feature.c
+++ lib/kfs_feature.c
@@ -6,4 +6,3 @@ const feature_t KFS_feature_nlinks = {id: 0x00000003, optional: 0, warn: 0, desc
 const feature_t KFS_feature_freespace = {id: 0x00000004, optional: 0, warn: 0, description: "Free Space On Disk"};
 const feature_t KFS_feature_file_lfs = {id: 0x00000005, optional: 0, warn: 0, description: "File Toplevel LFS"};
 const feature_t KFS_feature_file_lfs_name = {id: 0x00000006, optional: 0, warn: 0, description: "File Toplevel LFS Filename"};
-const feature_t KFS_feature_unixdir = {id: 0x00000007, optional: 0, warn: 0, description: "Unix style directories with . and .."};
