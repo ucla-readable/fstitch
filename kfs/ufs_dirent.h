4e2d8171dcb256bb30c1250c133d49c46a0afb05
Lei Zhang <leiz@featherstitch.ucla.edu>
2006-03-05 21:14:53 +0000

    r1966: Should have been in r1965.

--- kfs/ufs_dirent.h
+++ kfs/ufs_dirent.h
@@ -21,8 +21,7 @@
  *                   'dirf'.
  * find_free_dirent: Returns the offset into 'dirf' that has enough room to
  *                   write a directory entry of length 'len'.
- * insert_dirent:    Insert an entry named 'name' to 'dirf' at offset
- *                   'offset', with inode number 'ino' and file type 'type'.
+ * insert_dirent:    Insert an entry 'dirinfo' to 'dirf' at offset 'offset'.
  * delete_dirent:    Deletes the entry named 'name' from 'dirf'.
  * get_dirent:       Read an entry of up to 'size' bytes into 'entry' from
  *                   'dirf' starting at an offset described by 'basep'.
@@ -37,7 +36,7 @@ struct UFS_Dirent {
 	OBJECT(UFS_Dirent_t);
 	DECLARE(UFS_Dirent_t, int, search_dirent, ufs_fdesc_t * dirf, const char * name, inode_t * ino, int * offset);
 	DECLARE(UFS_Dirent_t, int, find_free_dirent, ufs_fdesc_t * dirf, uint32_t len);
-	DECLARE(UFS_Dirent_t, int, insert_dirent, ufs_fdesc_t * dirf, inode_t ino, uint8_t type, const char * name, int offset, chdesc_t ** head, chdesc_t ** tail);
+	DECLARE(UFS_Dirent_t, int, insert_dirent, ufs_fdesc_t * dirf, struct dirent dirinfo, int offset, chdesc_t ** head, chdesc_t ** tail);
 	DECLARE(UFS_Dirent_t, int, delete_dirent, ufs_fdesc_t * dirf, const char * name, chdesc_t ** head, chdesc_t ** tail);
 	DECLARE(UFS_Dirent_t, int, get_dirent, ufs_fdesc_t * dirf, struct dirent * entry, uint16_t size, uint32_t * basep);
 	DECLARE(UFS_Dirent_t, int, modify_dirent, ufs_fdesc_t * dirf, struct dirent entry, uint32_t basep, chdesc_t ** head, chdesc_t ** tail);
