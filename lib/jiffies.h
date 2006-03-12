56168fc9701607ae9f6b9ae5cda17128d80caecb
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2006-03-12 09:24:59 +0000

    r2018: Fixing the jiffies function in unixuser. This probably explains some of the strange timing differences between unixuser and KudOS kfsd.

--- lib/jiffies.h
+++ lib/jiffies.h
@@ -1,11 +1,14 @@
 #ifndef KUDOS_LIB_JIFFIES_H
 #define KUDOS_LIB_JIFFIES_H
 
-#if defined(UNIXUSER)
+#if defined(KUDOS)
+#define HZ			100
+#elif defined(UNIXUSER)
 // UNIXUSER only uses HZ for callbacks around every 1s or more
 #define HZ			5
+#elif defined(__KERNEL__)
 #else
-#define HZ			100
+#error Unknown target system
 #endif
 
 #define JIFFIES_PER_SECOND	HZ
@@ -34,7 +37,7 @@ static __inline int jiffy_time(void)
 		assert(0);
 	}
 	return tv.tv_sec * JIFFIES_PER_SECOND
-	       + tv.tv_usec * (1000000 / JIFFIES_PER_SECOND);
+	       + tv.tv_usec * JIFFIES_PER_SECOND / 1000000;
 }
 
 #elif defined(__KERNEL__)
