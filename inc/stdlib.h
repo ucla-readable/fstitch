9fa8ea793b8dea0210897bfe58dae2db62eaa652
Chris Frost <frost@cs.ucla.edu>
2006-03-14 02:20:38 +0000

    r2054: Add qsort prototype to kernel.

--- inc/stdlib.h
+++ inc/stdlib.h
@@ -9,7 +9,6 @@ void exit(int status);
 // Sort in ascending order. compar should return a value less than,
 // equal to, or greater than zero if 'a' is less than, equal to, or
 // greater than 'b', respectively.
-//#warning hello from qsort
 void qsort(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 
 #endif /* !KUDOS_INC_STDLIB_H */
--- lib/stdlib.h
+++ lib/stdlib.h
@@ -8,13 +8,17 @@
 #include <stdlib.h>
 
 #elif defined(__KERNEL__)
+
 #include <linux/slab.h>
 // For now use kmalloc(GFP_KERNEL), but this will need to change
 #define malloc(size) kmalloc(size, GFP_KERNEL)
 #define calloc(nmemb, size) kcalloc(nmemb, size, GFP_KERNEL)
 #define free(x) kfree(x)
 
-// TODO: qsort (could use xfs support's)
+// Sort in ascending order. compar should return a value less than,
+// equal to, or greater than zero if 'a' is less than, equal to, or
+// greater than 'b', respectively.
+void qsort(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 
 #else
 #error Unknown target system
