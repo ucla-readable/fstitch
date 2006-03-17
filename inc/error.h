83fe58a3afc5192fbe8886d42f69a711c7d82398
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2006-03-17 04:18:41 +0000

    r2074: Synchronizing KudOS error codes (with underscores) with Linux error codes (without underscores) when we compile for the kernel.

--- inc/error.h
+++ inc/error.h
@@ -3,6 +3,8 @@
 #ifndef KUDOS_INC_ERROR_H
 #define KUDOS_INC_ERROR_H
 
+#ifndef __KERNEL__
+
 // Error codes -- keep in sync with list in lib/printfmt.c.
 #define E_UNSPECIFIED	1	// Unspecified or unknown problem
 #define E_BAD_ENV       2       // Environment doesn't exist or otherwise
@@ -40,5 +42,29 @@
 #define E_NET_IF   28 // Net low-level netif error
 
 #define MAXERROR	28
+
+#else
+
+#include <linux/errno.h>
+
+/* Notice that E_UNSPECIFIED and E_EOF are the same value: neither really exists
+ * on Linux. Alo, this value happens to be EPERM, "operation not permitted". */
+#define E_UNSPECIFIED	1
+#define E_EOF		1
+#define E_INVAL		EINVAL
+#define E_FAULT		EFAULT
+#define E_NO_MEM	ENOMEM
+#define	E_NO_DISK	ENOSPC
+#define E_MAX_OPEN	ENFILE
+#define E_NOT_FOUND	ENOENT
+#define E_FILE_EXISTS	EEXIST
+#define E_NOT_EXEC	ENOEXEC
+#define E_NOT_DIR	ENOTDIR
+#define E_NOT_EMPTY	ENOTEMPTY
+#define E_BUSY		EBUSY
+#define E_NO_DEV	ENODEV
+#define E_PERM		EACCES /* Permission denied */
+
+#endif
 
 #endif	// !KUDOS_INC_ERROR_H */
