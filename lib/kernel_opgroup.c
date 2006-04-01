d8872b3cb3b65f4bbea7c5d44518f3d89c2d1c33
Chris Frost <frost@cs.ucla.edu>
2006-04-01 08:43:00 +0000

    r2188: Added debugging output

--- lib/kernel_opgroup.c
+++ lib/kernel_opgroup.c
@@ -1,15 +1,27 @@
 #include <errno.h>
 #include <kfs/opgroup.h>
 
+#define OPGROUP_DEBUG 0
+#if OPGROUP_DEBUG
+#include <stdio.h>
+#define Dprintf(x...) printf(x)
+#else
+#define Dprintf(x...) 
+#endif
+#define PREFIX "## "
+
 static int next = 1;
 
 opgroup_id_t opgroup_create(int flags)
 {
+	Dprintf("%s%s() = %d\n", PREFIX, __FUNCTION__, next);
 	return next++;
+
 }
 
 int opgroup_add_depend(opgroup_id_t dependent, opgroup_id_t dependency)
 {
+	Dprintf("%s%s(%d, %d)\n", PREFIX, __FUNCTION__, dependent, dependency);
 	if (dependent >= next || dependency >= next)
 		return -EINVAL;
 	return 0;
@@ -17,6 +29,7 @@ int opgroup_add_depend(opgroup_id_t dependent, opgroup_id_t dependency)
 
 int opgroup_engage(opgroup_id_t opgroup)
 {
+	Dprintf("%s%s(%d)\n", PREFIX, __FUNCTION__, opgroup);
 	if (opgroup >= next)
 		return -EINVAL;
 	return 0;
@@ -24,6 +37,7 @@ int opgroup_engage(opgroup_id_t opgroup)
 
 int opgroup_disengage(opgroup_id_t opgroup)
 {
+	Dprintf("%s%s(%d)\n", PREFIX, __FUNCTION__, opgroup);
 	if (opgroup >= next)
 		return -EINVAL;
 	return 0;
@@ -31,6 +45,7 @@ int opgroup_disengage(opgroup_id_t opgroup)
 
 int opgroup_release(opgroup_id_t opgroup)
 {
+	Dprintf("%s%s(%d)\n", PREFIX, __FUNCTION__, opgroup);
 	if (opgroup >= next)
 		return -EINVAL;
 	return 0;
@@ -38,6 +53,7 @@ int opgroup_release(opgroup_id_t opgroup)
 
 int opgroup_abandon(opgroup_id_t opgroup)
 {
+	Dprintf("%s%s(%d)\n", PREFIX, __FUNCTION__, opgroup);
 	if (opgroup >= next)
 		return -EINVAL;
 	return 0;
