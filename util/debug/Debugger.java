822b29859d2f2d6194c1e62e431ea70c36a46705
Chris Frost <frost@cs.ucla.edu>
2006-07-14 18:31:07 +0000

    r2636: Added debugRev r2612 to the supported list

--- util/debug/Debugger.java
+++ util/debug/Debugger.java
@@ -76,6 +76,8 @@ public class Debugger extends OpcodeFactory
 		/* supported revisions */
 		if(debugRev == 2584 && debugOpcodeRev == 2584)
 			return;
+		if(debugRev == 2612 && debugOpcodeRev == 2584)
+			return;
 		
 		/* 0 means "use a newer revision" */
 		throw new UnsupportedStreamRevisionException(debugRev, debugOpcodeRev, 0);
