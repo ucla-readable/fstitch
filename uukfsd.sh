11d41444ea4524be20aa3613ccef914854663f68
Chris Frost <frost@cs.ucla.edu>
2006-09-01 00:06:59 +0000

    r2695: - Add heap profiling KFSD_WRAP using valgrind's massif. uukfsd.sh config treats   hash_map..., vector..., and memdup as malloc functions by default. - Removed unnecessary suppressions option for cachegrind.

--- uukfsd.sh
+++ uukfsd.sh
@@ -28,8 +28,17 @@ then
 		|| ([ "$MNT" != "-h" ] && fusermount -uz "$MNT")
 elif [ "$KFSD_WRAP" == "cachegrind" ] && [ "$KFSD_WRAP_OPTS" == "" ]
 then
-	KFSD_WRAP_OPTS="--suppressions=conf/memcheck.supp --tool=cachegrind"
+	KFSD_WRAP_OPTS="--tool=cachegrind"
 	valgrind $KFSD_WRAP_OPTS "$KFSD" "$MNT" $KFSD_OPTS $@ \
+		|| ([ "$MNT" != "-h" ] && fusermount -uz "$MNT")
+elif [ "$KFSD_WRAP" == "massif" ] && [ "$KFSD_WRAP_OPTS" == "" ]
+then
+	KWO="--tool=massif"
+	KWO="$KWO --depth=5"
+	KWO="$KWO --alloc-fn=chain_elt_create --alloc-fn=hash_map_insert --alloc-fn=hash_map_resize --alloc-fn=hash_map_create_size --alloc-fn=hash_map_create"
+	KWO="$KWO --alloc-fn=vector_create_elts --alloc-fn=vector_create_size"
+	KWO="$KWO --alloc-fn=memdup"
+	valgrind $KWO "$KFSD" "$MNT" $KFSD_OPTS $@ \
 		|| ([ "$MNT" != "-h" ] && fusermount -uz "$MNT")
 elif [ "$KFSD_WRAP" == "time" ]
 then
