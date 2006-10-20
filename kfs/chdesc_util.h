25497912afa7587e2bb6d9d7902f3d2b9ed21f08
Chris Frost <frost@cs.ucla.edu>
2006-10-20 20:22:00 +0000

    r2763: Remove function prototypes for functions removed in r2757

--- kfs/chdesc_util.h
+++ kfs/chdesc_util.h
@@ -22,19 +22,8 @@ int chdesc_rewrite_block(bdesc_t * block, BD_t * owner, void * data, chdesc_t **
 /* reassign the block pointer in a NOOP chdesc */
 int chdesc_noop_reassign(chdesc_t * noop, bdesc_t * block);
 
-/* roll back and apply collections of change descriptors in the proper order */
-int chdesc_rollback_collection(int count, chdesc_t ** chdescs, void ** order);
-int chdesc_apply_collection(int count, chdesc_t ** chdescs, void ** order);
-void chdesc_order_destroy(void ** order);
-
-/* detach befores and afters of change descriptors */
-int chdesc_detach_befores(chdesc_t * chdesc);
-int chdesc_detach_afters(chdesc_t * chdesc);
-
-/* duplicate, split, and merge change descriptors */
+/* duplicate change descriptors */
 int chdesc_duplicate(chdesc_t * original, int count, bdesc_t ** blocks);
-int chdesc_split(chdesc_t * original, int count);
-int chdesc_merge(int count, chdesc_t ** chdescs, chdesc_t ** head);
 
 /* create change descriptors based on the diff of two data regions */
 int chdesc_create_diff(bdesc_t * block, BD_t * owner, uint16_t offset, uint16_t length, const void * olddata, const void * newdata, chdesc_t ** head);
