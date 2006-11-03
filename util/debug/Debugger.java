ad1d6a42fd7013739d7a7a112031b6c19ba6c4e0
Chris Frost <frost@cs.ucla.edu>
2006-11-03 06:41:26 +0000

    r2788: Update supported debug revisions

--- util/debug/Debugger.java
+++ util/debug/Debugger.java
@@ -92,6 +92,8 @@ public class Debugger extends OpcodeFactory
 		/* supported revisions */
 		if(debugRev == 2766 && debugOpcodeRev == 2766)
 			return;
+		if(debugRev == 2786 && debugOpcodeRev == 2766)
+			return;
 		
 		/* 0 means "use a newer revision" */
 		throw new UnsupportedStreamRevisionException(debugRev, debugOpcodeRev, 0);
