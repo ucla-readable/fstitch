4f45663f582bb345159b7d99658d11a4cd94893a
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-01-29 01:05:27 +0000

    r2855: Remove other platforms from lib/string.h.

--- lib/string.h
+++ lib/string.h
@@ -1,13 +1,6 @@
 #ifndef __KUDOS_LIB_STRING_H
 #define __KUDOS_LIB_STRING_H
 
-#if defined(KUDOS)
-#include <inc/string.h>
-
-#elif defined(UNIXUSER)
-#include <string.h>
-
-#elif defined(__KERNEL__)
 #include <lib/stdlib.h>
 #include <linux/string.h>
 
@@ -20,9 +13,5 @@ static __inline char * strdup(const char * s)
 	strcpy(c, s);
 	return c;
 }
-
-#else
-#error Unknown target system
-#endif
 
 #endif
