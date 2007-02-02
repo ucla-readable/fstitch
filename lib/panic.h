c6f596fd60c547fd921b86931e0187c8e344d1b3
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-02-02 02:55:47 +0000

    r2863: Removing unused stuff from lib/panic.h.

--- lib/panic.h
+++ lib/panic.h
@@ -1,23 +1,7 @@
 #ifndef KUDOS_LIB_PANIC_H
 #define KUDOS_LIB_PANIC_H
 
-#if defined(__KERNEL__)
-
 #include <linux/kernel.h>
-// Though kernel.h does not define warn or _warn, we use use neither
-
-#else
-
-#include <lib/stdio.h>
-#include <lib/assert.h>
-
-void _warn(const char*, int, const char*, ...);
-void _panic(const char*, int, const char*, ...) __attribute__((noreturn));
-
-#define warn(...) _warn(__FILE__, __LINE__, __VA_ARGS__)
-#define panic(...) _panic(__FILE__, __LINE__, __VA_ARGS__)
-
-#endif
 
 // static_assert(x) will generate a compile-time error if 'x' is false.
 #define static_assert(x)	switch (x) case 0: case (x):
