1c98709c44d8a1df8c93c3d172ebda73381a8a35
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-06-09 07:03:49 +0000

    r3496: Rebuild wbr_cache history step 1: delete. Will break compiles on this revision.

--- kfs/wbr_cache_bd.h
+++
@@ -1,8 +0,0 @@
-#ifndef __KUDOS_KFS_WBR_CACHE_BD_H
-#define __KUDOS_KFS_WBR_CACHE_BD_H
-
-#include <kfs/bd.h>
-
-BD_t * wbr_cache_bd(BD_t * disk, uint32_t soft_dblocks, uint32_t soft_blocks);
-
-#endif /* __KUDOS_KFS_WBR_CACHE_BD_H */
