3b3aa96b96a6884b2a57d91407dab7e4fb2a167e
Chris Frost <frost@cs.ucla.edu>
2007-07-01 03:27:45 +0000

    r3532: Correct a hash_map iteration to not modify its hash_map. (Surprisingly, this bug went unnoticed from its introduction in r2944 until a local inflight patch triggered it.) We may want to improve this fix to avoid its malloc and double iteration.

--- kfs/chdesc.c
+++ kfs/chdesc.c
@@ -1041,14 +1041,30 @@ static int chdesc_overlap_multiattach(chdesc_t * chdesc, bdesc_t * block)
 	}
 	else if(chdesc->type == BYTE && block->ddesc->bit_changes)
 	{
+		// TODO: avoid malloc and double iteration
 		hash_map_it_t it;
 		chdesc_t * bit_changes;
+		size_t n_bit_changes = hash_map_size(block->ddesc->bit_changes);
+		size_t i, n_overlaps = 0;
+		chdesc_t ** overlaps = smalloc(n_bit_changes * sizeof(*overlaps));
 		int r;
+		if(!overlaps)
+			return -ENOMEM;
+
+		// find the lists to multiattach, but do not multiattach during
+		// iteration since that might modify the hash_map
 		hash_map_it_init(&it, block->ddesc->bit_changes);
 		while((bit_changes = hash_map_val_next(&it)))
 			if(chdesc_overlap_check(chdesc, bit_changes->befores->before.desc))
-				if((r = _chdesc_overlap_multiattach(chdesc, bit_changes)) < 0)
-					return r;
+				overlaps[n_overlaps++] = bit_changes;
+
+		for(i = 0; i < n_overlaps; i++)
+			if((r = _chdesc_overlap_multiattach(chdesc, overlaps[i])) < 0)
+			{
+				sfree(overlaps, n_bit_changes * sizeof(*overlaps));
+				return r;
+			}
+		sfree(overlaps, n_bit_changes * sizeof(*overlaps));
 	}
 
 	// get range of buckets touched by this chdesc
