a6bf52d9fc1adcab00fe89f393b2887e5f4736c0
Mike Mammarella <mikem@featherstitch.cs.ucla.edu>
2007-08-30 20:29:11 +0000

    r4297: Fix debugger supported revision.

--- util/kdb.c
+++ util/kdb.c
@@ -374,7 +374,7 @@ static int read_debug_signature(void)
 	r = read_lit_32(&debug_opcode_rev);
 	if(r < 0)
 		return r;
-	if(debug_rev != 4258 || debug_opcode_rev != 4260)
+	if(debug_rev != 4289 || debug_opcode_rev != 4289)
 		return -EPROTO;
 	
 	r = read_lit_32(&initial_timestamp);
